817d77e6a9e3528a6b2f6af6adb8a48c7f81350f
lenonr <lenonrmsouza@gmail.com>
2016-02-10 19:44:23 -0200

    arquivo ini_neo4j.sh adicionado

---
+++ Scripts/other/ini_neo4j.sh
@@ -0,0 +1,9 @@
+#Para o Neo4j ser executado, deve ser necessário que ele esteja localizado na pasta Downloads
+clear
+echo 'Iniciando o aplicativo NEO4J , espere alguns segundos e inicie o seu navegador'
+echo
+echo 'Neste link -> localhost:7474'
+#echo
+echo '----------------------------------'
+cd Downloads/Neo4J/neo4j-community-2.3.0-M03/
+./bin/neo4j console
