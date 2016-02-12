78293eb3af38bbfb9f0025d000572d40a5c269cf
lenonr <lenonrmsouza@gmail.com>
2016-02-12 09:43:05 -0200

    arquivos modificados

--- Scripts/deb/ubuntu/atualizar.sh
+++ Scripts/deb/ubuntu/atualizar.sh
@@ -13,16 +13,14 @@ su -
 clear
 echo "É necessário atualizar a lista de pacotes do seu computador"
 read -n1 -p "Digite 's' para continuar ou 'n' para sair..." r
-
   apt-get update -y
 
 # atualizando versões dos programas
 clear
 echo "É necessário atualizar de programas do seu computador"
-
   apt-get upgrade -y
 
 # atualizando distribuição
 clear
 echo "É necessário tentar atualizar a sua distribuição"
-apt-get dist-upgrade
+  apt-get dist-upgrade
--- Scripts/other/battery_charge.sh
+++ Scripts/other/battery_charge.sh
@@ -4,10 +4,12 @@ echo "Script para verificar o consumo da bateria"
 echo
 echo "OBS: É necessário ter o Tlp instalado, para que o script possa ser executado corretamente"
 echo
-for i in {0..10};
+for i in {0..100};
 do
-  echo "$i´ verificacao"
+  clear
   sudo tlp stat -b | grep /sys/class/power_supply/BAT0/current_now
+  sudo tlp stat -b | grep /sys/class/power_supply/BAT0/charge_now
+  sleep 1
 done
 sleep 5
 clear
