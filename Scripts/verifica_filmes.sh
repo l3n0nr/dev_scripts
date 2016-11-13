7b4fa4b8311c7f8449d3f663a081273360faba5a
lenonr <lenonrmsouza@gmail.com>
2016-11-13 00:52:17 -0200

    script para verificar filmes adicionado

---
+++ Scripts/verifica_filmes.sh
@@ -0,0 +1,2 @@
+#script desenvolvido para gerar uma lista de filmes em um arquivo de texto, que o usuário possui na máquina para possiveis consultas.
+ls /media/lenonr/lenonr-500GB/Arquivos/Filmes/ | grep - > Lista_Filmes.txt
