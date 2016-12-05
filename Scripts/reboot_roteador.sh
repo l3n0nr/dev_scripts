da368487a1cb6528425dd4ae326d2bf9cf2717a4
lenonr <lenonrmsouza@gmail.com>
2016-12-04 22:54:55 -0200

    funcao reinicia roteador caso não exista internet, funcionando

--- Scripts/reboot_roteador.sh
+++ Scripts/reboot_roteador.sh
@@ -21,7 +21,8 @@ clear
 while true
 #i = 0;
 do 
-	echo "Aguardando tempo minimo para teste - 5 minutos" #sleep 300
+	echo "Aguardando tempo minimo para teste - 5 minutos" 
+	sleep 300
 	sleep 1
 	clear
 	#$i = $i+1;
@@ -29,7 +30,6 @@ do
 	internet=$(ping -c1 google.com.br | grep From | awk -F' ' '{ print $4 $5 $6}')
 	#echo $CMD 
 	if [ "$internet" == "DestinationPortUnreachable" ]; then
-		#se o ip não responder mostra offline!
 		echo "Offline!"
 		echo "Reiniciando o roteador, aguarde aproximadamente 1 minuto e meio para voltar a utilizar a Internet"
 		curl --user admin:admin http://192.168.11.1/userRpm/SysRebootRpm.htm?Reboot=Reboot
@@ -40,7 +40,6 @@ do
 		echo "Roteador funcionando! Teste.."
 		sleep 10
 	else
-		#se o ip responder o ping mostra online
 		echo "Online!" 
 		sleep 1
 	fi
