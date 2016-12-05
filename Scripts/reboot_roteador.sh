da9144f5c11b7a28ae282ac02ad0f8ee515b8eab
lenonr <lenonrmsouza@gmail.com>
2016-12-05 01:29:13 -0200

    corrigido bug bash

--- Scripts/reboot_roteador.sh
+++ Scripts/reboot_roteador.sh
@@ -1,4 +1,3 @@
-#!bin/bash
 ########################################################################
 # por carlosmorato
 # fonte: <https://www.vivaolinux.com.br/topico/Shell-Script/Script-para-reiniciar-Roteador-Apos-pingar
