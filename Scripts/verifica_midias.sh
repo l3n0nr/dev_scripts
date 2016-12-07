03b1af709213dd71582b1d5a7b35ddb30183a5b8
lenonr <lenonrmsouza@gmail.com>
2016-12-06 23:13:05 -0200

    alteração para salvar no mega os arquivos

--- Scripts/verifica_midias.sh
+++ Scripts/verifica_midias.sh
@@ -1,14 +1,14 @@
 #filmes
-	ls /media/lenonr/lenonr-500GB/Arquivos/Filmes/ | grep - > /media/lenonr/lenonr-500GB/Arquivos/Filmes/Lista_Filmes.txt
+	ls /media/lenonr/lenonr-500GB/Arquivos/Filmes/ | grep - > /home/lenonr/MEGA/Lista/Filmes.txt
 
 #seriados
-	tree -f /media/lenonr/lenonr-500GB/Arquivos/Seriados/ > /media/lenonr/lenonr-500GB/Arquivos/Seriados/Lista_Seriados.txt
+	tree -f /media/lenonr/lenonr-500GB/Arquivos/Seriados/ > /home/lenonr/MEGA/Lista/Seriados.txt
 	
 #musicas
-	tree -f /home/lenonr/Music/Musicas/ > /media/lenonr/lenonr-500GB/Arquivos/Outros/Lista_Musicas.txt	
+	tree -f /home/lenonr/Music/Musicas/ > /home/lenonr/MEGA/Lista/Musicas.txt	
 	
 #podcast's
-	tree -f /home/lenonr/Music/Podcast/ > /media/lenonr/lenonr-500GB/Arquivos/Outros/Lista_Podcast.txt
+	tree -f /home/lenonr/Music/Podcast/ > /home/lenonr/MEGA/Lista/Podcast.txt
 	
 #shows
-	ls /home/lenonr/Videos/Shows/ | grep - > /media/lenonr/lenonr-500GB/Arquivos/Outros/Lista_Shows.txt
+	ls /home/lenonr/Videos/Shows/ | grep - > /home/lenonr/MEGA/Lista/Shows.txt
