3e9b942685088063f9509d79ce42b8c4d9c9e033
lenonr <lenonrmsouza@gmail.com>
2016-12-10 16:48:35 -0200

    script melhorado

--- Scripts/remove_eclipse_atom.sh
+++
@@ -1,9 +0,0 @@
-clear
-echo "Removendo Eclipse"
-echo "----------------------------------------------"
-sudo apt-get autoremove eclipse* -y && rm -r ~/.eclipse/
-echo "Removendo Atom"
-echo "----------------------------------------------"
-sudo apt-get remove atom -y && sudo apt-get purge atom -y
-sleep 3
-echo "Desinstalação completa!"
--- Scripts/ubuntu/otimiza_recursos.sh
+++ Scripts/ubuntu/otimiza_recursos.sh
@@ -1,14 +1,18 @@
 clear
-#echo
-#echo "Digite a sua de administrador, para dar inicio ao script"
-#echo "---------------------------------------------------------------------"
-#su -
-echo
-echo "Ele deve ser executado em modo administrador"
-sleep 5
+#verificando se o usuário é ROOT
+if [[ `id -u` -ne 0 ]]; then
+	echo
+		echo "Você precisa ter poderes administrativos (root)"
+		echo "O script está sendo finalizado ..."
+		sleep 3
+		exit
+fi
 clear
-echo "Olá, este script foi criado para otimização de recursos em computadores"
-echo "NÃO É RECOMENTADO O SEU USO CASO ESTEJA REALIZANDO TAREFAS COMPLEXAS!"
+echo "Olá, este script foi criado para otimização de recursos em notebook's"
+echo "---------------------------------------------------------------------"
+echo "Seu uso é recomendado para utilização, quando deseja-se uma maior duração na bateria na leitura ou produção de textos"
+echo "Não é recomendado o uso, caso vá realizar tarefas complexas pois dessa forma ele irá prejudicar a sua bateria"
+echo "---------------------------------------------------------------------"
 echo
 echo "Você realmente dessa testá-lo? (s ou n)"
 echo "|---------------------------------------------------|"
@@ -31,7 +35,7 @@ if [ "$valor" == "s" ]; then
 		echo 0 >  /sys/class/rfkill/rfkill0/state
 	echo
 	echo "A partir de agora, o seu computador irá consumir menos recursos e talvez a sua bateria irá durar mais.."
-	#echo "Caso mantenha o uso do notebook constante e com poucas tarefas rodando em paralelo."
+	echo "Caso mantenha o uso do notebook constante e com poucas tarefas rodando em paralelo."
 	echo
 else
 	echo "Você acabou de desativar a otimização de recursos"
@@ -39,8 +43,8 @@ else
 	echo "| ======================================================= |"
 	echo "| Ativando nucleo do processador 1,2,3 respectivamente    |"
 		echo 1 >>  /sys/devices/system/cpu/cpu1/online
-	  echo 1 >>  /sys/devices/system/cpu/cpu2/online
-	  echo 1 >>  /sys/devices/system/cpu/cpu3/online
+	  	echo 1 >>  /sys/devices/system/cpu/cpu2/online
+	 	echo 1 >>  /sys/devices/system/cpu/cpu3/online
 	echo "| Ligando Wi-fi                                           |"
 	echo "| ======================================================= |"
 		echo 1 > /sys/class/rfkill/rfkill0/state
