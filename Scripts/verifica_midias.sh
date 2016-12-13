ddbcb6a663cd7fb6a8655d57639fa40ba020cdfa
lenonr <lenonrmsouza@gmail.com>
2016-12-12 23:42:00 -0200

    arquivos modificados

--- Scripts/verifica_midias.sh
+++ Scripts/verifica_midias.sh
@@ -1,26 +1,26 @@
 #filmes
 	echo "Verificando Filmes..."
-	ls /media/lenonr/lenonr-500GB/Arquivos/Filmes/ | grep - > /home/lenonr/MEGA/Lista/Filmes.txt
+	ls /media/lenonr/lenonr-500GB/Arquivos/Filmes/ | grep - > /home/lenonr/MEGA/Outros/Lista/Filmes.txt
 	clear
 
 #seriados
 	echo "Verificando Seriados"
-	tree -f /media/lenonr/lenonr-500GB/Arquivos/Seriados/ > /home/lenonr/MEGA/Lista/Seriados.txt
+	tree -f /media/lenonr/lenonr-500GB/Arquivos/Seriados/ > /home/lenonr/MEGA/Outros/Lista/Seriados.txt
 	clear
 	
 #musicas
 	echo "Verificando Musicas"
-	tree -f /home/lenonr/Music/Musicas/ > /home/lenonr/MEGA/Lista/Musicas.txt	
+	tree -f /home/lenonr/Music/Musicas/ > /home/lenonr/MEGA/Outros/Lista/Musicas.txt	
 	clear
 	
 #podcast's
 	echo "Verificando Podcast's"
-	tree -f /home/lenonr/Music/Podcast/ > /home/lenonr/MEGA/Lista/Podcast.txt
+	tree -f /home/lenonr/Music/Podcast/ > /home/lenonr/MEGA/Outros/Lista/Podcast.txt
 	clear
 	
 #shows
 	echo "Verificando Shows"
-	ls /home/lenonr/Videos/Shows/ | grep - > /home/lenonr/MEGA/Lista/Shows.txt
+	ls /home/lenonr/Videos/Shows/ | grep - > /home/lenonr/MEGA/Outros/Lista/Shows.txt
 	clear
 	
 	echo "Backup realizado! Script será finalizado em 5 segundos"
