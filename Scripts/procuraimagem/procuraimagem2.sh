1568d704a807359c60bab4ee8f78d1844e81dd6f
lenonr <lenonrmsouza@gmail.com>
2016-12-19 00:48:37 -0200

    script melhorado, trabalhando com variaveis

--- Scripts/procuraimagem/procuraimagem2.sh
+++ Scripts/procuraimagem/procuraimagem2.sh
@@ -19,30 +19,45 @@
 
 #limpando a tela
 clear
-touch .original.txt .removecaminho.txt .removewallhaven.txt .removeimagem.txt
+
+#criando arquivos
+#touch .original.txt .removecaminho.txt .removewallhaven.txt .removeimagem.txt
+
 #touch .area2.txt .base2.txt 
 
 #capturando caminho da imagem
 #xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace1/last-image > .area1.txt
+
 #xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace2/last-image > .area2.txt
 
-
 #descobrindo imagem original
-xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace1/last-image > .original.txt
+#url=`cat .numero.txt` 
+
+caminho=`xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace1/last-image | sed -e "s;/home/lenonr/MEGA/Imagens/;;g"`
+echo $caminho
+
+removejpg=`cat $caminho | sed -e "s;/*.jpg;;g"``
+echo $removejpg
+
+
+#xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace1/last-image > .original.txt
 
 #extraindo caminho base
-cat .original.txt | sed -e "s;/home/lenonr/MEGA/Imagens/Papeis de Parede/;;g" > .removecaminho.txt 
+#cat .original.txt | sed -e "s;/home/lenonr/MEGA/Imagens/Papeis de Parede/;;g" > .removecaminho.txt 
 
 #extraindo wallhaven
-cat .removecaminho.txt | sed -e "s;wallhaven-*;;g" > .removewallhaven.txt 
+#cat .removecaminho.txt | sed -e "s;wallhaven-*;;g" > .removewallhaven.txt 
 
 #extraindo imagem
-cat .removewallhaven.txt | sed -e "s;/*.jpg;;g" > .removeimagem.txt 
+#cat .removewallhaven.txt | sed -e "s;/*.jpg;;g" > .removeimagem.txt 
 
-cat .removeimagem.txt
+#cat .removeimagem.txt
 
-cat .base1.txt | sed -e "s;/wallhaven-;;g" > .base1.txt 
+#cat .base1.txt | sed -e "s;/wallhaven-;;g" > .base1.txt 
 
-cat .base1.txt | sed -e "s;"*"/;;g" > .base1.txt
+#cat .base1.txt | sed -e "s;'*'/;;g" > .base1.txt
 
-cat .base1.txt
+#cat .base1.txt
+
+#removendo arquivos
+#rm -r .original.txt .removecaminho.txt .removewallhaven.txt .removeimagem.txt
