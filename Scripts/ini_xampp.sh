fe165497832aeacf9e71f0e363a7a82115a04b9a
lenonr <lenonrmsouza@gmail.com>
2016-12-19 00:20:22 -0200

    script simplicado

--- Scripts/ini_xampp.sh
+++ Scripts/ini_xampp.sh
@@ -1,12 +1,10 @@
 clear
 echo "Iniciando Xampp"
 sudo /opt/lampp/lampp start
-sleep 2
+sleep 1
 clear
 echo "Abrindo Pasta do Xampp"
 thunar /opt/lampp/htdocs
-sleep 2
+sleep 1
 clear
-echo "Abrindo Firefox"
-firefox http://localhost/dev_bootstrap
-sleep 2
+exit
