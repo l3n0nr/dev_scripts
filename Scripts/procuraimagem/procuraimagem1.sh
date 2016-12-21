f74a05cbb27713db526bd5e7a1e88c4202a16830
lenonr <lenonrmsouza@gmail.com>
2016-12-21 02:35:01 -0200

    arquivo corrigido

--- Scripts/procuraimagem/procuraimagem1.sh
+++ Scripts/procuraimagem/procuraimagem1.sh
@@ -1,16 +1,17 @@
 	#limpando a tela
 	clear
+	
+	touch .caminho.txt .base.txt .numero.txt
 
 	#capturando caminho da imagem
  	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace1/last-image > .caminho.txt
 
-# 	#extraindo caminho base
- 	cat .caminho.txt | sed -e 
-"s;/home/lenonr/MEGA/Imagens/Pictures/People/wallhaven-;;g" > .base.txt 
+ 	#extraindo caminho base
+ 	cat .caminho.txt | sed -e "s;/home/lenonr/MEGA/Imagens/Pictures/People/wallhaven-;;g" > .base.txt 
  
-# 	#extraindo imagem
+ 	#extraindo imagem
 	cat .base.txt | sed -e "s;.jpg;;g" > .numero.txt 
 	url=`cat .numero.txt` 
 
 	#echo "ABRINDO IMAGEM NO WALLHAVEN"
-	firefox https://alpha.wallhaven.cc/wallpaper/$url
+ 	firefox https://alpha.wallhaven.cc/wallpaper/$url
