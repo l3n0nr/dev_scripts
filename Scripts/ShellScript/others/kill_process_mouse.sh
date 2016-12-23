89a0e1206880484934de8102cfcac497eb360c0d
lenonr <lenonrmsouza@gmail.com>
2016-12-23 10:25:49 -0200

    script's com objetivos variados

---
+++ Scripts/ShellScript/others/kill_process_mouse.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+clear
+echo "Matando processos que estão travando, clique em uma para fechá-la"
+echo "------------------------------------------------------------------"
+xkill
+close
---
+++ Scripts/ShellScript/others/verifica_midias.sh
@@ -0,0 +1,32 @@
+#filmes
+	echo "Verificando Filmes..."
+	ls /media/lenonr/lenonr-500GB/Arquivos/Filmes/ | grep - > /home/lenonr/MEGA/Outros/Lista/Filmes.txt
+	clear
+
+#seriados
+	echo "Verificando Seriados"
+	tree -f /media/lenonr/lenonr-500GB/Arquivos/Seriados/ > /home/lenonr/MEGA/Outros/Lista/Seriados.txt
+	clear
+	
+#documentarios
+	echo "Verificando Documentários"	
+	tree -f /media/lenonr/lenonr-500GB/Arquivos/Documentarios/ > /home/lenonr/MEGA/Outros/Lista/Documentarios.txt
+	clear
+	
+#musicas
+	echo "Verificando Musicas"
+	tree -f /home/lenonr/Music/Musicas/ > /home/lenonr/MEGA/Outros/Lista/Musicas.txt	
+	clear
+	
+#podcast's
+	echo "Verificando Podcast's"
+	tree -f /home/lenonr/Music/Podcast/ > /home/lenonr/MEGA/Outros/Lista/Podcast.txt
+	clear
+	
+#shows
+	echo "Verificando Shows"
+	ls /home/lenonr/Videos/Shows/ | grep - > /home/lenonr/MEGA/Outros/Lista/Shows.txt
+	clear
+	
+	echo "Backup realizado! Script será finalizado em 5 segundos"
+	sleep 5
