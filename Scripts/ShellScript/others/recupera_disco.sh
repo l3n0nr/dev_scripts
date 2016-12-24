223b42ea76fe64d274857bef59e17a3fe499549f
lenonr <lenonrmsouza@gmail.com>
2016-12-24 09:56:27 -0200

    script melhorado, adicionada versao

--- Scripts/ShellScript/others/recupera_disco.sh
+++ Scripts/ShellScript/others/recupera_disco.sh
@@ -1,1 +1,56 @@
-sudo fsck /dev/sda2 -y -C -f && sudo fsck /dev/sda3 -y -C -f && reboot
+#!/bin/bash
+#
+####################
+# DESENVOLVIDO POR #
+####################
+#
+# por lenonr(Lenon Ricardo) 
+# 	contato: <lenonrmsouza@gmail.com>
+#
+#################################################################################
+#										#
+#	If I have seen further it is by standing on the shoulders of Giants.	#
+#	(Se vi mais longe foi por estar de pé sobre ombros de gigantes)		#
+#							~Isaac Newton		#
+#										#
+#################################################################################
+#
+##################################
+# versão do script: 0.0.15.0.0.0 #
+##################################
+#
+# legenda: a.b.c.d.e.f
+# 	a = alpha[0], beta[1];
+# 	b = erros na execução;	
+# 	c = interações com o script + versões funcionando;
+# 	d = correções necessárias;
+# 	e = pendencias
+# 	f = desenvolver
+#
+################################################################################
+#
+# Script testado em
+#	-Xubuntu 16.04
+#
+################################################################################
+#
+################################################################################
+# FUNCOES
+#   -Realizar correções em partições com erro, indicado para partições Linux;
+#
+################################################################################
+#
+#limpando a tela
+clear
+
+#verificando a particao alvo
+sudo fdisk -l
+
+particao=`/dev/sdc1`
+
+#tentando corrigir
+sudo fsck $particao -y -C -f 
+
+#reiniciando a maquina
+#reboot
+################################################################################
