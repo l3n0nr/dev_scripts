2206d71e2ddff82d8543220e306e3467ebd1fd62
lenonr <lenonrmsouza@gmail.com>
2016-12-24 16:53:48 -0200

    comentarios adicionados

--- Scripts/ShellScript/searchimage_wallhaven/procuraimagem1.sh
+++ Scripts/ShellScript/searchimage_wallhaven/procuraimagem1.sh
@@ -1,21 +1,63 @@
-	#limpando a tela
-	clear
-	
-	#criandos arquivos para manipulação
-	touch .caminho.txt .base.txt .numero.txt
+#!/bin/bash
+#
+####################
+# DESENVOLVIDO POR #
+####################
+#
+# por lenonr(Lenon Ricardo) 
+# 	contato: <lenonrmsouza@gmail.com> 
+#
+#################################################################################
+#										#
+#	If I have seen further it is by standing on the shoulders of Giants.	#
+#	(Se vi mais longe foi por estar de pé sobre ombros de gigantes)		#
+#							~Isaac Newton		#
+#										#
+#################################################################################
+#
+##################################
+# versão do script: 1.0.22.0.0.0 #
+##################################
+#
+# legenda: a.b.c.d.e.f
+# 	a = alpha[0], beta[1];
+# 	b = erros na execução;	
+# 	c = interações com o script + versões funcionando;
+# 	d = correções necessárias;
+# 	e = pendencias
+# 	f = desenvolver
+#
+################################################################################
+#
+# Script testado em
+#	-Xubuntu 16.04
+#
+################################################################################
+#
+################################################################################
+# FUNCOES
+#   -Captura imagem do wallhaven de plano de fundo e abre diretamente no site
+#
+################################################################################
 
-	#capturando caminho da imagem
- 	xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace1/last-image > .caminho.txt
+#limpando a tela
+clear
 
- 	#extraindo caminho base
- 	cat .caminho.txt | sed -e "s;/home/lenonr/MEGA/Imagens/Pictures/People/wallhaven-;;g" > .base.txt 
- 
- 	#extraindo imagem
-	cat .base.txt | sed -e "s;.jpg;;g" > .numero.txt 
-	url=`cat .numero.txt` 
+#criandos arquivos para manipulação
+touch .caminho.txt .base.txt .numero.txt
 
-	#echo "ABRINDO IMAGEM NO WALLHAVEN"
- 	firefox https://alpha.wallhaven.cc/wallpaper/$url
- 	
- 	#removendo arquivos criados
- 	rm .caminho.txt .base.txt .numero.txt
+#capturando caminho da imagem
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace1/last-image > .caminho.txt
+
+#extraindo caminho base
+cat .caminho.txt | sed -e "s;/home/lenonr/MEGA/Imagens/Pictures/People/wallhaven-;;g" > .base.txt 
+
+#extraindo imagem
+cat .base.txt | sed -e "s;.jpg;;g" > .numero.txt 
+url=`cat .numero.txt` 
+
+#echo "ABRINDO IMAGEM NO WALLHAVEN"
+firefox https://alpha.wallhaven.cc/wallpaper/$url
+
+#removendo arquivos criados
+rm .caminho.txt .base.txt .numero.txt
