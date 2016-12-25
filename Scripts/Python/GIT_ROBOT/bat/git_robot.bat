564f9c0e2a7d816ba48b81475b24683a185f00ee
lenonr <lenonrmsouza@gmail.com>
2016-12-25 18:29:42 -0200

    projeto para automizar ações no Git

---
+++ Scripts/Python/GIT_ROBOT/bat/git_robot.bat
@@ -0,0 +1,3 @@
+@echo off
+
+python C:\git_robot\git_robot.py %1
---
+++ Scripts/Python/GIT_ROBOT/git_robot/git_conf.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#=================================#
+# Arquivo de configuracoes
+#=================================#
+
+
+#==============================#
+# Define aqui a branch em 
+# que se esta trabalhando
+#==============================#
+ORIGIN = "master"
+#==============================#
+
+#==============================#
+# Define aqui o respositorio 
+# que se esta trabalhando
+#==============================#
+REPOSITORY = "https://AndersonFirmino@bitbucket.org/nome_do_projeto/git_do_projeto.git"
+#==============================#
