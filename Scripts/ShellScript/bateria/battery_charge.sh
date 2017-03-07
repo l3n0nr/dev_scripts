4c003e11581418edec6838d4bfa9a94e2b1c07e2
lenonr <lenonrmsouza@gmail.com>
2017-03-07 12:58:11 -0300

    arquivos modificados

--- Scripts/ShellScript/bateria/battery_charge.sh
+++ Scripts/ShellScript/bateria/battery_charge.sh
@@ -1,15 +1,10 @@
+# !bin/bash
+#limpando a tela
 clear
-echo
-echo "Script para verificar o consumo da bateria"
-echo
-echo "OBS: É necessário ter o Tlp instalado, para que o script possa ser executado corretamente"
-echo
-for i in {0..1000};
+
+while true
+    clear
+    sudo tlp stat -b | grep /sys/class/power_supply/BAT0/current_now
+    sudo tlp stat -b | grep /sys/class/power_supply/BAT0/charge_now
+    sleep 5
 do
-	  clear
-	  sudo tlp stat -b | grep /sys/class/power_supply/BAT0/current_now
-	  sudo tlp stat -b | grep /sys/class/power_supply/BAT0/charge_now
-	sleep 1
-	done
-sleep 5
-clear
--- Scripts/ShellScript/reboot/swap.sh
+++ Scripts/ShellScript/reboot/swap.sh
@@ -1,7 +1,7 @@
 #desligando e ligado a swap no modo administrador
 clear
-echo "Limpando a memoria Swap.."
 echo "Desligando a memoria Swap.."
+echo "Limpando a memoria Swap.."
 echo "--------------------------"
 sudo swapoff -a && sudo swapon -a
 echo "Ligando a memoria Swap.."
