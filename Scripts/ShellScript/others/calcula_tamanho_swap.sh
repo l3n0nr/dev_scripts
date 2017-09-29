9b79ac1e0e671890f0fa66e931fbbe9f50ac94e4
lenonr <lenonrmsouza@gmail.com>
2017-09-29 16:12:29 -0300

    arquivo melhor

--- Scripts/ShellScript/others/calcula_tamanho_swap.sh
+++ Scripts/ShellScript/others/calcula_tamanho_swap.sh
@@ -12,6 +12,9 @@
 #echo
 
 clear
+echo "CALCULADO DE MEMÓRIA SWAP"
+echo 
+
 #capturando a quantidade de memoria
 tam_mem=$(free -mt | grep Mem: | awk '{ print $2 " MB" }' | sed -e "s; MB;;")
 echo "Tamanho da memória é $tam_mem MB"
@@ -29,26 +32,35 @@ echo "Tamanho da memória é $tam_mem MB"
 # se valor menor que 2GB
 if [ "$tam_mem" -lt "2048" ]; then
     # calculando tamanho swap
-    var=$((($tam_mem * 5)/10))
+    swap=$((($tam_mem * 5)/10))
     # var_teste=$(($tam_mem - $var))
 
     # mostrando valor
-    echo "Tamanho da memória SWAP recomendada: $var MB"	
+    echo "Tamanho da memória SWAP recomendada: $swap MB"	        
 
 elif [ "$tam_mem" -lt "4092" ]; then
     # calculando tamanho swap
-    var=$((($tam_mem * 3)/10))
+    swap=$((($tam_mem * 3)/10))
     # var_teste=$(($tam_mem - $var))
 
     # mostrando valor
-    echo "Tamanho da memória SWAP recomendada: $var MB"
+    echo "Tamanho da memória SWAP recomendada: $swap MB"
+
+    echo     
+    # realizando soma das memorias
+    total=$(($tam_mem+$swap))
+    
+    # mostrand memoria total do computador
+    echo "O seu computador ira conter $total MB de memória total."
+    echo 
+    
 else
     # calculando tamanho swap
-    var=$((($tam_mem * 1)/10))
+    swap=$((($tam_mem * 1)/10))
     # var_teste=$(($tam_mem - $var))
 
     # mostrando valor
-    echo "Tamanho da memória SWAP recomendada: $var MB"
+    echo "Tamanho da memória SWAP recomendada: $swap MB"
 fi
 
 # echo "Teste: $var_teste MB"
