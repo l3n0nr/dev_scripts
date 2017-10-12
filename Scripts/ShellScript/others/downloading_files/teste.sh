5a1174ef48d692ed9fd5178bae5974c65ef3d462
lenonr <lenonrmsouza@gmail.com>
2017-10-12 07:10:57 -0300

    download of the links by recursive form

---
+++ Scripts/ShellScript/others/downloading_files/teste.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+num=00
+while [ $num -le 73 ]
+do
+#         wget http://reader.mundoavatar.com.br/content/comics/korra-1-guerras-territoriais_5912667965f23/1-0_591268612be8f/Page $num.jpg
+#         wget http://reader.mundoavatar.com.br/content/comics/avatar-1-a-promessa_59121ff9d9238/1-0_591223357e4ab/Avatar%20-%20The%20Last%20Airbender%20-%20The%20Promise%20Part%201-0$num.jpg
+#         wget http://reader.mundoavatar.com.br/content/comics/avatar-1-a-promessa_59121ff9d9238/2-0_591234c1992a9/Avatar%20-%20The%20Last%20Airbender%20-%20The%20Promise%20Part%202-0$num.jpg
+#         wget http://reader.mundoavatar.com.br/content/comics/avatar-1-a-promessa_59121ff9d9238/3-0_591235f353448/Avatar%20-%20The%20Last%20Airbender%20-%20The%20Promise%20Part%203-00$num.jpg
+#         wget http://reader.mundoavatar.com.br/content/comics/avatar-2-a-busca_5912203f64c96/1-0_59123914446d5/Avatar%20-%20The%20Last%20Airbender%20-%20The%20Search%20Part%201-0$num.jpg
+#         wget http://reader.mundoavatar.com.br/content/comics/avatar-2-a-busca_5912203f64c96/2-0_59123aae2bc29/The%20Search%20Part%202_$num.jpg
+        wget http://reader.mundoavatar.com.br/content/comics/avatar-2-a-busca_5912203f64c96/3-0_5912403ac5fd4/000$num.jpg
+	(( num++ ))
+done
+
