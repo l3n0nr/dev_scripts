95fd1005d0c89f56497d75633f137ff340dd1d74
lenonr <lenonrmsouza@gmail.com>
2017-11-01 11:52:28 -0200

    script de NOOB - basico

---
+++ Scripts/Python/cozinha/tempo_cozinha.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#-*- coding: UTF-8 -*-
+
+import os, sys
+
+####	CABEÇALHO DO SCRIPT
+## SCRIPT PARA CALCULAR O TEMPO MEDIO(IDEAL) PARA O PREPARO DE UMA REFEIÇÃO
+#
+### CRIANDO LOGICA
+# TEMPO DE PREPARO DA REFEIÇÃO, NAO PODE SER MAIOR DO QUE TEMPO MAXIMO DE CONSUMO.
+# EXEMPLO: TEMPO PARA ALMOÇAR, 30 MINUTOS ENTÃO TEMPO MAXIMO(IDEAL) PARA PREPARO DO ALMOÇO É DE NO MAXIMO 1 HORA(IDEAL)
+
+
+####	CORPO DO SCRIPT
+# funcao para calculo
+
+## criando variaveis
+#def calcula_alimentacao()
+#{
+tipo_alimentacao = raw_input('Digite qual a refeicao que voce ira realizar:')
+print tipo_alimentacao
+tempo_alimentacao= input("Digite o tempo que voce leva para realizar o ? tipo_alimentacao")
+print tempo_alimentacao
+#}
+#tempo_preparo
+
+## realizando comparação
+#if var1:
+#   print "1 - Got a true expression value"
+#   print var1
+#else:
+#   print "1 - Got a false expression value"
+#   print var1
+
+####	RODAPE DO SCRIPT
+
