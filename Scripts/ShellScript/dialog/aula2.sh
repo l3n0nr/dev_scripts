c5124b38f1c46fb4c9c83fedd76659c7161300a7
lenonr <lenonrmsouza@gmail.com>
2017-11-04 09:47:35 -0200

    exercicio proposto criado

--- Scripts/ShellScript/dialog/aula2.sh
+++ Scripts/ShellScript/dialog/aula2.sh
@@ -9,30 +9,48 @@
 ############################################
 #
 # criando variaveis para serem utilizadas no script
-back_title = "Caixa de informação do programa"
+#back_title = "Caixa de informação do programa"
 #
 ############################################
 ## CORPO DO SCRIPT
 # caixa de entrada - entrada de dados simples
-nome=$(dialog --stdout --title "Caixa de Nome" --backtitle "$backtitle" \
-	--inputbox "Digite o seu nome" 0 0)
+#nome=$(dialog --stdout --title "Caixa de Nome" --backtitle "$backtitle" \
+#	--inputbox "Digite o seu nome" 0 0)
 
 # caixa de entrada - modo password
-senha=$(dialog --stdout --title "Caixa de Senha" --backtitle "$backtitle" \
-	--insecure --passwordbox "Digite a sua senha" 0 0)
+#senha=$(dialog --stdout --title "Caixa de Senha" --backtitle "$backtitle" \
+#	--insecure --passwordbox "Digite a sua senha" 0 0)
 
-# mostrando nome digitado
-dialog  --title "Bem vindo!" --backtitle "$backtitle" \
-	--sleep 3 \
-	--infobox "$nome" 0 0
+## mostrando nome digitado
+#dialog  --title "Bem vindo!" --backtitle "$backtitle" \
+#	--sleep 3 \
+#	--infobox "$nome" 0 0
 
 # mostrando senha digitada
-dialog  --title "Sua senha é" --backtitle "$backtitle" \
-	--sleep 3 \
-	--infobox "$senha" 0 0
+#dialog  --title "Sua senha é" --backtitle "$backtitle" \
+#	--sleep 3 \
+#	--infobox "$senha" 0 0
 
 #echo "$nome"
 #echo "$senha"
+
+#### EXERCICIO PROPOSTOo
+# criando variavel de titulo da janela
+back_title="Tela de Login"
+
+# criando dialog's
+# caixa de nome
+nome=$(dialog  --stdout --title "Login" --backtitle "$back_title" \
+	--inputbox "Digite o seu nome" 0 0)
+
+# caixa de senha
+senha=$(dialog --stdout --title "Login" --backtitle "$back_title" \
+	--insecure --passwordbox "Digite a sua senha" 0 0)
+
+# mensagem nome e senha - mostrando o usuario
+dialog --title "Usuario e senha do sistema" --backtitle "$back_title" \
+	--sleep 5 \
+	--infobox "\n Usuário: $nome; Senha: $senha" 5 50
 #
 ############################################
 ## RODAPE DO SCRIPT
