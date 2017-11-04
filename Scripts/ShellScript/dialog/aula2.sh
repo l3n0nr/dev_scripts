867ca1e10cece036f8dd5b91f0fd892410629bb2
lenonr <lenonrmsouza@gmail.com>
2017-11-04 08:21:08 -0200

    arquivo melhorado

--- Scripts/ShellScript/dialog/aula2.sh
+++ Scripts/ShellScript/dialog/aula2.sh
@@ -7,18 +7,29 @@
 # Titulo Raio X-dialog - Aula 2
 #
 ############################################
+#
+# criando variaveis para serem utilizadas no script
+back_title = "Caixa de informação do programa"
+#
+############################################
 ## CORPO DO SCRIPT
 # caixa de entrada - entrada de dados simples
-nome=$(dialog --stdout --title "Caixa de Nome" --backtitle "Caixa de informações do programa" \
+nome=$(dialog --stdout --title "Caixa de Nome" --backtitle "$backtitle" \
 	--inputbox "Digite o seu nome" 0 0)
 
 # caixa de entrada - modo password
-senha=$(dialog --stdout --title "Caixa de Senha" --backtitle "Caixa de informações do programa" \
-	--passwordbox "Digite a sua senha" 0 0)
+senha=$(dialog --stdout --title "Caixa de Senha" --backtitle "$backtitle" \
+	--insecure --passwordbox "Digite a sua senha" 0 0)
 
 # mostrando nome digitado
-dialog  --title "Bem vindo!" --backtitle "Caixa de Informação" --sleep 3 \
+dialog  --title "Bem vindo!" --backtitle "$backtitle" \
+	--sleep 3 \
 	--infobox "$nome" 0 0
+
+# mostrando senha digitada
+dialog  --title "Sua senha é" --backtitle "$backtitle" \
+	--sleep 3 \
+	--infobox "$senha" 0 0
 
 #echo "$nome"
 #echo "$senha"
