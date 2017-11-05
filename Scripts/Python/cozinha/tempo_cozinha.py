975e2c67ff978c2c75502854e19a9be8a546601f
lenonr <lenonrmsouza@gmail.com>
2017-11-05 15:51:25 -0200

    calculo funcionando

--- Scripts/Python/cozinha/tempo_cozinha.py
+++ Scripts/Python/cozinha/tempo_cozinha.py
@@ -4,33 +4,30 @@
 import os, sys
 
 ####	CABEÇALHO DO SCRIPT
-## SCRIPT PARA CALCULAR O TEMPO MEDIO(IDEAL) PARA O PREPARO DE UMA REFEIÇÃO
+## SCRIPT PARA CALCULAR O TEMPO MEDIO(IDEAL) PARA O PREPARO DE UMA REFEIÇÃO PARA UM COZINHEIRO PREGUIÇO, BUSCANDO ASSIM, OTIMIZAR SEU TEMPO.
 #
 ### CRIANDO LOGICA
-# TEMPO DE PREPARO DA REFEIÇÃO, NAO PODE SER MAIOR DO QUE TEMPO MAXIMO DE CONSUMO.
-# EXEMPLO: TEMPO PARA ALMOÇAR, 30 MINUTOS ENTÃO TEMPO MAXIMO(IDEAL) PARA PREPARO DO ALMOÇO É DE NO MAXIMO 1 HORA(IDEAL)
-
-
+# TEMPO DE PREPARO DA REFEIÇÃO NAO PODE SER MAIOR DO QUE O DOBRO, DO TEMPO MAXIMO DE CONSUMO.
+# EXEMPLO: TEMPO PARA ALMOÇAR, 30 MINUTOS ENTÃO TEMPO MAXIMO(IDEAL) PARA PREPARO DO ALMOÇO É DE NO MAXIMO 1 HORA.
+#
 ####	CORPO DO SCRIPT
-# funcao para calculo
+#
+# limpando tela
+os.system('cls' if os.name == 'nt' else 'clear')
 
-## criando variaveis
-#def calcula_alimentacao()
-#{
-tipo_alimentacao = raw_input('Digite qual a refeicao que voce ira realizar:')
-print tipo_alimentacao
-tempo_alimentacao= input("Digite o tempo que voce leva para realizar o ? tipo_alimentacao")
-print tempo_alimentacao
-#}
-#tempo_preparo
+# mostrando mensgem inicial
+print "Script para calculo do tempo ideal para preparo de uma refeição. Seja bem vindo(a)!"
 
-## realizando comparação
-#if var1:
-#   print "1 - Got a true expression value"
-#   print var1
-#else:
-#   print "1 - Got a false expression value"
-#   print var1
+# nova linha
+print
+
+# criando variaveis
+tipo_alimentacao = raw_input("Digite qual a refeicao que voce ira realizar: ")
+tempo_alimentacao= input("Digite o tempo(medio) que voce leva para consumir essa refeição? ")
+
+# nova linha
+print
+print "Caso voce vá preparar o(a)", tipo_alimentacao, "seria interessante voce preparar em", tempo_alimentacao*2, "minutos(tempo maximo)"
 
 ####	RODAPE DO SCRIPT
-
+# Sim eu sim, é um script de noob! porem foi criado para aprendizagem na linguagem python, tenha paciencia!
