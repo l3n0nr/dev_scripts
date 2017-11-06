dfa83ab91ec79418bad7c6e74036332f790c495b
lenonr <lenonrmsouza@gmail.com>
2017-11-05 22:14:50 -0200

    funcoes do script adicionado

--- Scripts/Python/lista_ep_series/lista_episodios.py
+++ Scripts/Python/lista_ep_series/lista_episodios.py
@@ -4,6 +4,11 @@
 #	 CABEÇALHO
 # Referência: <https://www.vivaolinux.com.br/script/Organizando-suas-musicas-com-Python/>
 # Autor Original: Emanuel <https://www.vivaolinux.com.br/~manel_>
+# 
+# OBJETIVOS DO SCRIPT
+# OBJ01: Este script possui o objetivo de buscar a lista complete de ep. da serie digitada pela usuario.
+# OBJ02: Dessa forma pretende-se utilizar o arquivo gerado para renomear arquivos pre-existentes em uma pasta.
+
 ###############
 #	CORPO
 from os import rename
