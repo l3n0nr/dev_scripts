dbbd3c1184eb111cb7f664a66e1b4158e10ee0b1
lenonr <lenonrmsouza@gmail.com>
2017-12-23 21:24:35 -0200

    aula 3 adicionada

---
+++ Scripts/Python/cursos/AprendaCodigos/aula3.py
@@ -0,0 +1,6 @@
+# Referência: <youtu.be/MvEYbThCVeU?list=PLV7VqBqvsd_3yRYYWrHkziPL6izzrUIkp>
+
+nome = input("Qual é o seu nome? ")
+
+# imprimindo string entre aspas
+print ('O seu nome é "'+ nome +'"')
