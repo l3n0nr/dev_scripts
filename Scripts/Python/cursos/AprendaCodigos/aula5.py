195ec0f410b2f1c6b3e84ab7ef7e43812ae2b9a5
lenonr <lenonrmsouza@gmail.com>
2018-02-12 19:28:22 -0200

    aula 5 adicionada

---
+++ Scripts/Python/cursos/AprendaCodigos/aula5.py
@@ -0,0 +1,10 @@
+# Referencia
+# Titulo: Como criar um módulo de Python 3 e importar em outro arquivo
+# Link: <https://youtu.be/GyshQ8W7z9I?list=PLV7VqBqvsd_3yRYYWrHkziPL6izzrUIkp>
+
+
+# criando modulo
+import aula4
+
+# chamando funcao
+aula4.nome
