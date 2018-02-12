60cf9355628b2e327da506182d03d881682484e1
lenonr <lenonrmsouza@gmail.com>
2018-02-12 19:28:14 -0200

    aula 4 adicionada

---
+++ Scripts/Python/cursos/AprendaCodigos/aula4.py
@@ -0,0 +1,19 @@
+#######################################
+# Referência
+#	Titulo: Como criar uma definição (função) no Python 3
+#	Link: <https://youtu.be/uDW2mbWEQu8?list=PLV7VqBqvsd_3yRYYWrHkziPL6izzrUIkp>
+#######################################
+
+# limpa a tela
+import os
+os.system('clear')
+
+# criando funcao para ler e mostrar nome
+def nome():
+
+	nome = input("Qual é o seu nome: ")
+	print('Bem-vindo '+nome+'!')
+
+# executando funcao
+nome()
+
