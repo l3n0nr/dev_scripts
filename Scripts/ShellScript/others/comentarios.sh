95dfd5716e02fc243582912167fb509bbf91d3ff
Lenon Ricardo <lenonr@desktop>
2018-03-28 14:36:12 -0300

    comentarios sobre comentarios - shell script

---
+++ Scripts/ShellScript/others/comentarios.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+#
+#========================================================#
+# Referencia: 
+#	COMENTÁRIOS - Shell Script
+#	<https://www.youtube.com/watch?v=8cRgYbAMzY8>
+#========================================================#
+# Trabalhando com comentarios nos codigos em ShellScript 
+#
+# Procurar definir uma margem(coluna) padrao nos comentarios
+# para evitar cansaço na leitura, alem para evitar conflitos
+# na indentaçao nos editores.
+#
+# COMENTARIOS DE LINHA
+# blablabla
+#
+# COMENTARIOS EM BLOCO
+#========================================================#
+#
+#========================================================#
+#
