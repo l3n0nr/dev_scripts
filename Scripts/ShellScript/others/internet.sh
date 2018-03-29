e17056cf4d98619e15ab91b0daf11f99c96148e3
Lenon Ricardo <lenonr@desktop>
2018-03-29 01:50:52 -0300

    arquivo melhorado - break adicionado

--- Scripts/ShellScript/others/internet.sh
+++ Scripts/ShellScript/others/internet.sh
@@ -14,26 +14,27 @@
 #
 # variaveis de ambiente
 SERVIDOR="google.com"						# servidor para teste
-TEMPO=60s									# tempo de intervalo do teste(segundos)
+# TEMPO=60s									# tempo de intervalo do teste(segundos)
 
 # realiza teste enquanto valor de internet for "0"(falso)	
 while true; do	
 	# intervalo de tempo 
-	sleep $TEMPO
+	# sleep $TEMPO
 
 	# ====================================== #
 	# testando conexao internet
 	# ====================================== #	
-	ping -c 5 $SERVIDOR >>/dev/null;
+	ping -c 10 $SERVIDOR >>/dev/null;
 
 	# verificando valor
 	if [[ $? = "0" ]]; then				
 		# echo "Internet funcionando!" >> /tmp/internet_log.txt
 		# notify-send -u normal "Internet funcionando" -t 2500
-		printf ""
+		printf "OK\n"
+		break;
 	else				
 		# mostrando mensagem na tela
-		notify-send -u normal "Sem Conexao?!" -t 5000
+		notify-send -u normal "Sem Conexao no link $SERVIDOR!" -t 5000
 		# echo "Sem Internet!" >> /tmp/internet_log.txt		
 	fi	
 	# ====================================== #
