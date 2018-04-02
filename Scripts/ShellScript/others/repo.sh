20f4edb742e3412656c7bd184bd82f187e9d669d
lenonr <lenonrmsouza@gmail.com>
2018-04-02 11:00:53 -0300

    while implementado

--- Scripts/ShellScript/others/repo.sh
+++ Scripts/ShellScript/others/repo.sh
@@ -22,44 +22,51 @@
 
 # # variaveis
 # pasta dos repositorios
-LOCAL='/home/lenonr/Github/'		
+LOCAL='/home/lenonr/Github/'		# pasta do repositorio
 
 # repositorios disponiveis
-REPOS=(dev_xfce dev_scripts dev_ksp dev_sysadmin dev_web dev_clonerepo)		
+REPOS=(dev_xfce dev_scripts dev_ksp dev_sysadmin dev_web dev_clonerepo)		# repositorios
+
+# intervalo de tempo
+TEMPO=14400s						# sera executado 3 vezes por dia a cada 4 horas
 
 pull_git()
 {
-	# # walk to the array
-	for (( i = 1; i <= ${#REPOS[@]}; i++ )); do	
-		# verify local repo disk
-		if [[ $LOCAL${REPOS[$i]} != $LOCAL ]]; then
-			# verify local repo
-			if [ -e "$LOCAL${REPOS[$i]}" ]; then 	  	 
-				printf ""
-			  	echo "[+] - Atualizando repositorio:" $LOCAL${REPOS[$i]} >> /tmp/repo.txt
+	# intervalo de tempo 
+	while true; do	
+		sleep $TEMPO
 
-			  	# into folder location
-			  	cd $LOCAL${REPOS[$i]}
+		# # walk to the array
+		for (( i = 1; i <= ${#REPOS[@]}; i++ )); do	
+			# verify local repo disk
+			if [[ $LOCAL${REPOS[$i]} != $LOCAL ]]; then
+				# verify local repo
+				if [ -e "$LOCAL${REPOS[$i]}" ]; then 	  	 
+					date >> /tmp/repo.txt
+				  	echo "[+] - Atualizando repositorio:" $LOCAL${REPOS[$i]} >> /tmp/repo.txt
 
-			  	# update repositories
-			  	git pull >> /tmp/repo.txt
+				  	# into folder location
+				  	cd $LOCAL${REPOS[$i]}
 
-			  	# REPO_FOUNDS=$(($REPO_FOUNDS + 1));        
-			  	let REPO_FOUNDS++		  	
-			# else
-			# 	printf ""
-			# 	echo "[-] - Not found": $LOCAL${REPOS[$i]}
-			# 	printf "Download now!\n"			
-			# 	git clone $SERVER${REPOS[$i]}$GIT
+				  	# update repositories
+				  	git pull >> /tmp/repo.txt
 
-			# 	# REPO_NOTFOUNDS=$(($REPO_NOTFOUNDS + 1));        
-			# 	let REPO_NOTFOUNDS++
+				  	# REPO_FOUNDS=$(($REPO_FOUNDS + 1));        
+				  	let REPO_FOUNDS++		  	
+				else
+					date >> /tmp/repo.txt
+					echo "[-] - Not found": $LOCAL${REPOS[$i]}
+
+					# REPO_NOTFOUNDS=$(($REPO_NOTFOUNDS + 1));        
+					let REPO_NOTFOUNDS++
+				fi
 			fi
-		fi
-	done	
+		done	
+	done
 }
 
 # data de inicio do script
+echo "Inicio do script" > /tmp/repo.txt
 date > /tmp/repo.txt
 
 # chamando funcao
