35ffc6442dd64b23e5fee298e2e3324ee9612cfb
lenonr <lenonrmsouza@gmail.com>
2018-04-02 10:45:56 -0300

    arquivo melhorado - funcao funcionando

--- Scripts/ShellScript/others/internet.sh
+++ Scripts/ShellScript/others/internet.sh
@@ -9,49 +9,56 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # #
 # # data de criação do script:    [28/03/18]      #
-# # ultima ediçao realizada:      [30/03/18]      #
+# # ultima ediçao realizada:      [01/04/18]      #
 # # # # # # # # # # # # # # # # # # # # # # # # # #
-#
-# Instruções
-# ##########
-# 
-# O script pode ser utilizado via crontab através de:
-# */15 * * * * sh /location_folder/internet.sh 
-#				# executando script a cada 15 minutos
 #
 #
 # variaveis de ambiente
-SERVIDOR="google.com"						# servidor para teste
-# TEMPO=60s									# tempo de intervalo do teste(segundos)
+SERVIDOR="pool.ntp.br"						# servidor para teste
+TEMPO=300s									# tempo de intervalo do teste(segundos) - 5 minutos
 
 # realiza teste enquanto valor de internet for "0"(falso)	
 func_internet()
 {
 	# intervalo de tempo 
-	# sleep $TEMPO
+	while true; do	
+		sleep $TEMPO
 
-	# date >> /tmp/internet_log.txt
+		# date >> /tmp/internet_log.txt
 
-	# ====================================== #
-	# testando conexao internet
-	# ====================================== #	
-	ping -q -c1 $SERVIDOR > /dev/null
+		# ====================================== #
+		# testando conexao internet
+		# ====================================== #	
+		ping -q -c1 $SERVIDOR > /dev/null
+		# ping -q -c1 $SERVIDOR >> /tmp/internet.txt
 
-	# verificando valor
-	# if [[ $? = "0" ]]; then	
-	if [ $? = "0" ]; then	
-		MENSAGEM="Internet funcionando!"
-		notify-send -u normal "$MENSAGEM" -t 2500
-	else				
-		MENSAGEM="Sem conexao ao link $SERVIDOR!"
-		notify-send -u normal "$MENSAGEM" -t 5000		
-		# firefox $SERVIDOR
-	fi	
+		# verificando valor
+		# if [[ $? = "0" ]]; then	
+		if [ $? = "0" ]; then	
+			MENSAGEM="Internet funcionando!"
+			echo $MENSAGEM >> /tmp/internet.txt
+			date >> /tmp/internet.txt
 
-	# mostrando mensagem
-	# echo $MENSAGEM >> /tmp/internet_log.txt
-	# ====================================== #
+			# notify-send -u normal "$MENSAGEM" -t 2500
+		else				
+			MENSAGEM="Sem conexao ao link $SERVIDOR!"
+			date >> /tmp/internet.txt
+			echo $MENSAGEM >> /tmp/internet.txt
+
+			notify-send -u normal "$MENSAGEM" -t 5000		
+			# firefox $SERVIDOR
+		fi	
+
+		# mostrando mensagem
+		# echo $MENSAGEM >> /tmp/internet_log.txt
+		# ====================================== #
+	done
 }
+
+echo "Inicio do script" > /tmp/internet.txt
+date >> /tmp/internet.txt
 
 # chamando funcao
 func_internet
+
+date >> /tmp/internet.txt
