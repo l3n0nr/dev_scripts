e2341eca41b8a6e3a52094c08b8dad31e60b2fc4
lenonr <lenonrmsouza@gmail.com>
2018-04-02 10:28:39 -0300

    arquivo melhorado - funcao funcionando

--- Scripts/ShellScript/others/repo.sh
+++ Scripts/ShellScript/others/repo.sh
@@ -17,106 +17,53 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # ## # # #
 # Date create script:    	  		[30/03/18]       #
-# Last modification script: 		[30/03/18]       #
+# Last modification script: 		[01/04/18]       #
 # # # # # # # # # # # # # # # # # # # # # # # ## # # #
 
-# variables
-LOCAL_DEVXFCE='/home/lenonr/Github/dev_xfce'
-LOCAL_DEVSCRIPTS='/home/lenonr/Github/dev_scripts'
-LOCAL_DEVSYSADMIN='/home/lenonr/Github/dev_sysadmin'
-LOCAL_DEVKSP='/home/lenonr/Github/dev_ksp'
-LOCAL_DEVWEB='/var/www/html/dev_web'
-LOCAL_DEVCLONEREPO='/home/lenonr/Github/dev_clonerepo'
+# # variaveis
+# pasta dos repositorios
+LOCAL='/home/lenonr/Github/'		
 
-# # implementar array para otimizar script
-# REPOS=(dev_xfce dev_scripts dev_ksp dev_sysadmin dev_web dev_clonerepo)
-
-# # # walk to the array
-# for (( i = 0; i <= ${#REPOS[@]}; i++ )); do	
-# 	# verify local repo disk
-# 	if [[ $LOCAL${REPOS[$i]} != $LOCAL ]]; then
-# 		# verify local repo
-# 		if [ -e "$LOCAL${REPOS[$i]}" ]; then 	  	 
-# 			printf ""
-# 		  	echo "[+] - Found:" $LOCAL${REPOS[$i]}
-
-# 		  	# into folder location
-# 		  	cd $LOCAL${REPOS[$i]}
-
-# 		  	# update repositories
-# 		  	git pull
-
-# 		  	# REPO_FOUNDS=$(($REPO_FOUNDS + 1));        
-# 		  	let REPO_FOUNDS++		  	
-# 		else
-# 			printf ""
-# 			echo "[-] - Not found": $LOCAL${REPOS[$i]}
-# 			printf "Download now!\n"			
-# 			git clone $SERVER${REPOS[$i]}$GIT
-
-# 			# REPO_NOTFOUNDS=$(($REPO_NOTFOUNDS + 1));        
-# 			let REPO_NOTFOUNDS++
-# 		fi
-# 	fi
-# done	
+# repositorios disponiveis
+REPOS=(dev_xfce dev_scripts dev_ksp dev_sysadmin dev_web dev_clonerepo)		
 
 pull_git()
 {
-	clear 	
-	  
-  	# verificando se diretorio existe 
-  	if [ -e "$LOCAL_DEVXFCE" ]; then 
-	  	echo "[*] Verificando Diretório dev_xfce"
-	  	cd $LOCAL_DEVXFCE
-	  	git pull
-	  	printf "\n"
-	fi
+	# # walk to the array
+	for (( i = 1; i <= ${#REPOS[@]}; i++ )); do	
+		# verify local repo disk
+		if [[ $LOCAL${REPOS[$i]} != $LOCAL ]]; then
+			# verify local repo
+			if [ -e "$LOCAL${REPOS[$i]}" ]; then 	  	 
+				printf ""
+			  	echo "[+] - Atualizando repositorio:" $LOCAL${REPOS[$i]} >> /tmp/repo.txt
 
-	# verificando se diretorio existe 
-  	if [ -e "$LOCAL_DEVSCRIPTS" ]; then 
-	  	echo "[*] Verificando Diretório dev_scripts"
-	  	cd $LOCAL_DEVSCRIPTS
-	  	git pull
-	  	printf "\n"
-	fi
+			  	# into folder location
+			  	cd $LOCAL${REPOS[$i]}
 
-	# verificando se diretorio existe 
-  	if [ -e "$LOCAL_DEVSYSADMIN" ]; then 
-	  	echo "[*] Verificando Diretório dev_sysadmin"
-	  	cd $LOCAL_DEVSYSADMIN
-	  	git pull
-	  	printf "\n"
-	fi
+			  	# update repositories
+			  	git pull >> /tmp/repo.txt
 
-	# verificando se diretorio existe 
-  	if [ -e "$LOCAL_DEVKSP" ]; then 
-	  	echo "[*] Verificando Diretório dev_ksp"
-	  	cd $LOCAL_DEVKSP
-	  	git pull
-	  	printf "\n"
-	fi
+			  	# REPO_FOUNDS=$(($REPO_FOUNDS + 1));        
+			  	let REPO_FOUNDS++		  	
+			else
+				printf ""
+				echo "[-] - Not found": $LOCAL${REPOS[$i]}
+				printf "Download now!\n"			
+				git clone $SERVER${REPOS[$i]}$GIT
 
-	# verificando se diretorio existe 
-  	if [ -e "$LOCAL_DEVWEB" ]; then 
-	  	echo "[*] Verificando Diretório dev_web"
-	  	cd $LOCAL_DEVWEB
-	  	git pull
-	  	printf "\n"
-	fi
-
-	# verificando se diretorio existe 
-  	if [ -e "$LOCAL_DEVCLONEREPO" ]; then 
-	  	echo "[*] Verificando Diretório dev_clonerepo"
-	  	cd $LOCAL_DEVCLONEREPO
-	  	git pull
-	  	printf "\n"
-	fi
-
-	# verificando se diretorio existe 
-  	if [ -e "$LOCAL_HOME" ]; then 
-	  	cd $LOCAL_HOME
-	fi	
+				# REPO_NOTFOUNDS=$(($REPO_NOTFOUNDS + 1));        
+				let REPO_NOTFOUNDS++
+			fi
+		fi
+	done	
 }
+
+# data de inicio do script
+date > /tmp/repo.txt
 
 # chamando funcao
 pull_git
+
+# data do final do script
+date >> /tmp/repo.txt
