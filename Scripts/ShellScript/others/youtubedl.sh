d63885e77109a26c508679d7f19f9d7c9cc365ef
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-06-14 22:47:45 -0300

    conversor video youtube para mp3 adicionado

---
+++ Scripts/ShellScript/others/youtubedl.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#                           CABEÇALHO DO SCRIPT                               #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# # # # # # # # # # # # #
+#   FONTES DE PESQUISA  #
+# # # # # # # # # # # # #
+#
+# Slackjeff 
+# Source: <https://notabug.org/jeffersonrocha/youtube2mp3>
+#
+# # # # # # # # # # # # #
+#   DESENVOLVIDO POR    #
+# # # # # # # # # # # # #
+#
+# por lenonr(Lenon Ricardo)
+#       contato: <lenonrmsouza@gmail.com>
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#									      									  #
+#	If I have seen further it is by standing on the shoulders of Giants.      #
+#	(Se vi mais longe foi por estar de pé sobre ombros de gigantes)	          #
+#							~Isaac Newton	      							  #
+#									      									  #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # #
+# # data de criação do script:    [14/06/18]      #             
+# # ultima ediçao realizada:      [14/06/18]      #
+# # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Legenda: a.b.c.d.e.f
+# 	a = alpha[0], beta[1], stable[2], freeze[3];
+# 	b = erros na execução;
+# 	c = interações com o script;
+# 	d = correções necessárias; 
+# 	e = pendencias
+# 	f = desenvolver
+#
+# variaveis do script
+#
+# contatos do autor
+    email="lenonrmsouza@gmail.com"   
+    twitter="twitter.com/lenonr1"
+
+# versao do script
+    versao="0.0.10.0.0.0"  
+
+# variaveis	
+	format="mp3"	# "aac", "flac", "mp3", "m4a",  "opus",  "vorbis",  or  "wav"
+	quality="320k"	
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#                           CORPO DO SCRIPT                               	  #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# definindo funcoes
+f_youtube-dl()
+{
+	# convertendo link
+	youtube-dl --embed-thumbnail --audio-quality "$quality"  --extract-audio --audio-format "$format" "$@"						
+}
+
+main()
+{
+	# verificando parametros
+	[[ $# -eq 0 ]] && printf "Chame o script e cole um link de video do Youtube!" && exit 1 \
+				   || f_youtube-dl $@
+}
+
+## chamando funcao principal
+main $@
+
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+#                           RODAPE DO SCRIPT                                  #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
