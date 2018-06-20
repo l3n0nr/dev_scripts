c63e6821b54d2470590ecc5b389f1c32879aea73
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-06-20 17:40:34 -0300

    arquivos melhorados - bateria notebook

--- Scripts/ShellScript/others/interface_sys.sh
+++ Scripts/ShellScript/others/interface_sys.sh
@@ -2,11 +2,15 @@
 
 
 # DESCRICAO DO SCRIPT:
-# Otimiza recursos da maquina(notebook)
-# 	- Desliga/Liga nucleos extras(1-2-3) # 0, nunca e desligado
+# Otimiza recursos da maquina, para economizar bateria(notebook)
+# 	- Desliga/Liga nucleos extras(1-2-3) # Nucleo 0 nunca e desligado
 # 	- Desliga/Liga Wifi
-# 	- Reduz a 0/Aumenta a 100 o Brilho
+# 	- Reduz/Aumenta o brilho
 
+## variaveis do script
+hostname=$(echo $HOSTNAME)
+
+## funcao de verificacao
 f_verifica()
 {
 	[[ $? == "1" ]] && \
@@ -14,18 +18,43 @@ f_verifica()
 			   --text "Script finalizado, antes do esperado!" && exit 1
 }
 
+## funcao nucleos
 f_cpu()
 {
-	if [[ $otimiza == "Ligar" ]]; then
-		echo 1 >> /sys/devices/system/cpu/cpu1/online
-		echo 1 >> /sys/devices/system/cpu/cpu2/online
-		echo 1 >> /sys/devices/system/cpu/cpu3/online
-	elif [[ $otimiza == "Desligar" ]]; then
+	if [[ $otimiza == "Otimizar" ]]; then
+		echo "| ======================================================= |"
+		echo "| Desativando nucleo do processador 1,2,3 respectivamente |"
 		echo 0 >> /sys/devices/system/cpu/cpu1/online
 		echo 0 >> /sys/devices/system/cpu/cpu2/online
-		echo 0 >> /sys/devices/system/cpu/cpu3/online		
+		echo 0 >> /sys/devices/system/cpu/cpu3/online       
+		echo "| ======================================================= |"
+	elif [[ $otimiza == "Default" ]]; then
+		echo "| ======================================================= |"
+		echo "| Ativando nucleo do processador 1,2,3 respectivamente    |"
+		echo 1 >>  /sys/devices/system/cpu/cpu1/online
+	  	echo 1 >>  /sys/devices/system/cpu/cpu2/online
+	 	echo 1 >>  /sys/devices/system/cpu/cpu3/online
+	 	echo "| ======================================================= |"
 	else
-		echo "Nao entendi!"
+		echo "Nao entendi, CPU!"
+	fi
+}
+
+## funcao wifi
+f_wifi()
+{
+	if [[ $otimiza == "Otimizar" ]]; then
+		echo "| ======================================================= |"
+		echo "| Desativando Wifi|"
+		echo 0 >  /sys/class/rfkill/rfkill0/state
+		echo "| ======================================================= |"
+	elif [[ $otimiza == "Default" ]]; then
+		echo "| ======================================================= |"
+		echo "| Ativando Wifi|"
+		echo 1 >  /sys/class/rfkill/rfkill0/state
+		echo "| ======================================================= |"
+	else
+		echo "Nao entendi, WIFI!"
 	fi
 }
 
@@ -38,18 +67,26 @@ f_notebook()
 		   --height "200" \
 		   --column "" --column="Marque" \
 		   --radiolist \
-	            FALSE Desligar \
-	            FALSE Ligar    
-	)
+	            FALSE Default \
+	            FALSE Otimizar    
+	) 
 
-	# f_cpu
-	f_verifica
+	f_cpu	
+	f_wifi
 }
 
+## funcao principal
 main()
 {
-	# f_interface
-	f_notebook
+	# verifica se root
+	[[ `id -u` -ne 0 ]] && \
+    	zenity --info --text "Root e necessario" && \
+    	exit 1    	
+
+    ## verifica se e notebook
+	[[ ! $hostname -eq "notebook" ]] && echo "Nao serve!" && exit 1 \
+									 || f_notebook
 }
 
+## chamando script
 main
