377394a0f532b5361af5114c04d7edd7a9c912b1
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-09-13 14:59:13 -0300

    arquivo melhorado

--- Scripts/ShellScript/others/pomodoro.sh
+++ Scripts/ShellScript/others/pomodoro.sh
@@ -1,22 +1,53 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# DESCRICAO
+# Simula o aplicativo pomodoro para controle das tarefas
+
+# variaveis do programa
+inicia="Iniciando o pomodoro com o tempo de"
+finaliza="Acabou a sua sessao!"
 
 pomodoro()
 {
-	while true; do
-		notify-send $mensagem
+	if [[ $1 == "" ]]; then
+		echo "Por favor, passo como parametro um tempo em minutos"
+		echo "Exemplo: $0 10"
+	else
+		tempo="$(($1*60))"
+
+		echo $inicia $tempo "segundos"
+		echo "Contando.."
 		sleep $tempo
-	done		
+
+		notify-send -t 5000 $finaliza
+	fi	
 }
 
 main()
 {
-	[[ $1 -eq "" ]] || [[ $2 -eq "" ]] && \
-		tempo=15 && mensagem="Hora de se esticar um pouco" || \
-		tempo=$1 && mensagem=$2 \
-
-	tempo_con="$tempo"m	
-
-	pomodoro
+	pomodoro $1
 }
 
-main $1 $2
+# chamando funcao principal
+main $1
+
+# pomodoro()
+# {
+# 	while true; do
+# 		notify-send $mensagem
+# 		sleep $tempo
+# 	done		
+# }
+
+# main()
+# {
+# 	[[ $1 -eq "" ]] || [[ $2 -eq "" ]] && \
+# 		tempo=15 && mensagem="Hora de se esticar um pouco" || \
+# 		tempo=$1 && mensagem=$2 \
+
+# 	tempo_con="$tempo"m	
+
+# 	pomodoro
+# }
+
+# main $1 $2
