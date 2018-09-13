7c85cbc690854720a6ad265f953b86101dcbb9ae
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-09-13 15:03:12 -0300

    arquivo otimizado

--- Scripts/ShellScript/others/pomodoro.sh
+++ Scripts/ShellScript/others/pomodoro.sh
@@ -1,53 +1,34 @@
 #!/usr/bin/env bash
 #
+# ULT_MOD: 13/09/18
 # DESCRICAO
-# Simula o aplicativo pomodoro para controle das tarefas
+# Simula o aplicativo pomodoro para controle de tempo na execucao das tarefas
 
 # variaveis do programa
-inicia="Iniciando o pomodoro com o tempo de"
-finaliza="Acabou a sua sessao!"
+mensagem_inicia="Iniciando o pomodoro com"
+mensagem_finaliza="Acabou a sua sessao!"
 
 pomodoro()
 {
-	if [[ $1 == "" ]]; then
-		echo "Por favor, passo como parametro um tempo em minutos"
-		echo "Exemplo: $0 10"
-	else
-		tempo="$(($1*60))"
+	tempo="$(($1*60))"
 
-		echo $inicia $tempo "segundos"
-		echo "Contando.."
-		sleep $tempo
+	echo $mensagem_inicia $tempo "segundos!"
+	echo "Contando.."
+	sleep $tempo
 
-		notify-send -t 5000 $finaliza
-	fi	
+	echo $mensagem_finaliza 
 }
 
 main()
 {
-	pomodoro $1
+	clear
+	if [[ $1 == "" ]]; then
+		echo "Por favor, passo como parametro um tempo em minutos"
+		echo "Exemplo: $0 10"
+	else				
+		pomodoro $1
+	fi
 }
 
 # chamando funcao principal
 main $1
-
-# pomodoro()
-# {
-# 	while true; do
-# 		notify-send $mensagem
-# 		sleep $tempo
-# 	done		
-# }
-
-# main()
-# {
-# 	[[ $1 -eq "" ]] || [[ $2 -eq "" ]] && \
-# 		tempo=15 && mensagem="Hora de se esticar um pouco" || \
-# 		tempo=$1 && mensagem=$2 \
-
-# 	tempo_con="$tempo"m	
-
-# 	pomodoro
-# }
-
-# main $1 $2
