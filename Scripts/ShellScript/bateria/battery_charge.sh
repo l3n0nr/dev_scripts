f8c269bb0b3a51ea56abee23beff0a8305717562
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-10-08 16:24:47 -0300

    verificar cut ou awk no arquivo

--- Scripts/ShellScript/bateria/battery_charge.sh
+++ Scripts/ShellScript/bateria/battery_charge.sh
@@ -1,32 +1,47 @@
 #!/usr/bin/env bash
 #
-## DESCRICAO
-# Verifica a quantidade de bateria disponivel
+## DESCRICAO: Verifica informacoes da bateria via TLP
+## COMPATIVEL: Debian Stable
+
+f_tlpcheck()
+{
+	which /usr/sbin/tlp >> /dev/null
+
+	if [[ $? -eq 1 ]]; then
+		echo "[-] Voce precisa executar ~apt install tlp -y~ para prosseguir"
+		exit 1
+	fi
+}
 
 f_command()
 {
-	tlp stat -b | grep /sys/class/power_supply/BAT0/current_now
-	tlp stat -b | grep /sys/class/power_supply/BAT0/charge_now
+	tlp stat -b | grep "Charge"
+	tlp stat -b | grep "current_now"
+	# tlp stat -b | grep /sys/class/power_supply/BAT0/current_now | cut -d: -f1
+	tlp stat -b | grep /sys/class/power_supply/BAT0/charge_now 
 }
 
 check_battery()
 {
+	## executa em loop ate o usuario cancelar
 	while [[ TRUE ]]; do
 		clear
 		f_command
 
+		# aguarda e executa novamente ou sai
 		[[ $? -eq 0 ]] && sleep 1 || exit 1
 	done
 }
 
 main()
 {
-	clear
+	clear	
 
 	if [[ `id -u` -ne 0 ]]; then
 		echo "[-] PRECISA DE ROOT PARA EXECUTAR" && exit 1
 	fi
 
+	f_tlpcheck
 	check_battery
 }
 
