a1fbd055c18821ca911ee3bb0225d52d58761419
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-10-08 11:05:56 -0300

    arquivo melhorado

--- Scripts/ShellScript/bateria/battery_charge.sh
+++ Scripts/ShellScript/bateria/battery_charge.sh
@@ -6,12 +6,13 @@
 f_command()
 {
 	tlp stat -b | grep /sys/class/power_supply/BAT0/current_now
-	# tlp stat -b | grep /sys/class/power_supply/BAT0/charge_now
+	tlp stat -b | grep /sys/class/power_supply/BAT0/charge_now
 }
 
 check_battery()
 {
 	while [[ TRUE ]]; do
+		clear
 		f_command
 
 		[[ $? -eq 0 ]] && sleep 1 || exit 1
@@ -21,6 +22,10 @@ check_battery()
 main()
 {
 	clear
+
+	if [[ `id -u` -ne 0 ]]; then
+		echo "[-] PRECISA DE ROOT PARA EXECUTAR" && exit 1
+	fi
 
 	check_battery
 }
