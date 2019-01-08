e901bbe6eebcdd1f3a9e23cb58155c3714cd3ff2
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-08 10:51:50 -0200

    testar no notebook

--- Scripts/ShellScript/bateria/check_time.sh
+++ Scripts/ShellScript/bateria/check_time.sh
@@ -2,6 +2,30 @@
 #
 ###################################
 # CALCULA TEMPO RESTANTE DE BATERIA
+########################
 #
-########s#
+# DAT_CRIAC	:	07/01/19
+# LAST_MOD	:	08/01/19
+# VERSAO	:	0.5
+# AUTOR 	:	lenonr
+#
+########################
+#
 # ($charge_now * 60) / $current_now
+
+check()
+{
+	tlp stat -b | grep /sys/class/power_supply/BAT0/charge_now | awk ('print $4')
+}
+
+## funcao principal
+main()
+{
+	clear	
+
+	if [[ `id -u` -ne 0 ]]; then
+		echo "[-] PRECISA DE ROOT PARA EXECUTAR" && exit 1
+	fi
+	
+	check
+}
