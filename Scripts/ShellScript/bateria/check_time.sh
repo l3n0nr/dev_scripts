a2410aac80bffeb886cfcbfe1450bada04747de8
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-10 15:42:43 -0200

    verificar calculo date

--- Scripts/ShellScript/bateria/check_time.sh
+++ Scripts/ShellScript/bateria/check_time.sh
@@ -6,8 +6,9 @@
 ###################################
 #
 # DAT_CRIAC	:	07/01/19
-# LAST_MOD	:	09/01/19
-# VERSAO	:	0.20
+# LAST_MOD	:	10/01/19
+# VERSAO	:	0.35
+#		BUG: Verificar calculo de tempo da bateria via date
 # AUTOR 	:	lenonr
 #
 ########################
@@ -15,45 +16,71 @@
 # VARIAVEIS
 aguarda="1"
 
-status="$(cat /sys/class/power_supply/BAT0/status)"
-full_battery="$(($(cat /sys/class/power_supply/BAT0/charge_full) / 1000))"
-charge_now="$(($(cat /sys/class/power_supply/BAT0/charge_now) / 1000))"
-current_now="$(($(cat /sys/class/power_supply/BAT0/current_now) / 1000))"
-battery_res="$((($full_battery*60)/$current_now))"
-
-battery_res_h="$(($battery_res / 60 ))"
-perc_batery="$(((($charge_now * 100)) / $full_battery))"
-
-low_res="$($full_battery * 40)"
-med_res="$($full_battery * 60)"
-high_res="$($full_battery * 80)"
-
 check()
 {
-	## verificar comando
-	# upower -i /org/freedesktop/UPower/devices/battery_BAT0
+	status="$(cat /sys/class/power_supply/BAT0/status)"
+	full_battery="$(($(cat /sys/class/power_supply/BAT0/charge_full) / 1000))"
+	charge_now="$(($(cat /sys/class/power_supply/BAT0/charge_now) / 1000))"
+	current_now="$(($(cat /sys/class/power_supply/BAT0/current_now) / 1000))"
 
-	echo "############################################"
-	echo "Verified in:" $(date +%kh:%mm:%Ss)
-	echo "########################"
-	echo "Status battery:" $status
-	echo "Charge battery now:" $charge_now "mAh"
-	echo "Current resources now:" $current_now "mA"
+	battery_res="$((($full_battery * 60) / $current_now))"
 
-	echo "Time rest:" $battery_res "min /" $battery_res_h "h / " $perc_batery "%"
-	# echo "Shutdown in time(-):" $(date -d $battery_res +%kh:%mm)
+	battery_res_h="$(($battery_res / 60))"
+	perc_batery="$(((($charge_now * 100)) / $full_battery))"
+	battery_full="$((($full_battery*60)/$current_now))"
+
+	calc_time=$(($battery_full - $battery_res))
+
+	low_res="$((($full_battery * 40) / 100))"
+	med_res="$((($full_battery * 60) / 100))"
+	high_res="$((($full_battery * 80) / 100))"
+
+	# date_rest="$(($(echo $battery_res / 6) | bc))"
+	date_rest="$battery_res"
+
+	# echo $battery_res
+	# echo $date_rest
 
 	# echo $low_res
 	# echo $med_res
 	# echo $high_res
 
-	# if [[ $battery_now > $low_res ]] && [[ $battery_now < $med_res ]] ; then
-	# 	echo "Consuming: LOW."
-	# elif [[ $battery_now > $med_res ]] && [[ $battery_now < $hig_res ]] ; then
-	# 	echo "Consuming: MEDIUM."
-	# else
-	# 	echo "Consuming: HIGH."
-	# fi
+	# echo "############################################"
+	# echo "Verified in:" $(date +%kh:%mm:%Ss)
+	# echo "########################"
+	# echo "Status battery:" $status	
+	# echo "Charge battery now:" $charge_now "mAh"
+	# echo "Current now:" $current_now "mA"		
+
+	if [[ $current_now -le $low_res ]] && [[ $current_now -lt $med_res ]] && [[ $current_now -lt $high_res ]] ; then
+		consuming_level="LOW"
+	elif [[ $current_now -lt $low_res ]] && [[ $current_now -ge $med_res ]] && [[ $current_now -lt $high_res ]] ; then
+		consuming_level="MEDIUM"
+	elif [[ $current_now -lt $low_res ]] && [[ $current_now -lt $med_res ]] && [[ $current_now -ge $high_res ]] ; then
+		consuming_level="HIGH"
+	else
+		consuming_level="ERROR $current_now"		
+	fi
+
+	echo "############################################"
+
+	if [[ $status == "Discharging" ]]; then						
+		# echo "Verified in:" $(date +%kh:%mm:%Ss)
+		# echo "########################"
+		echo "Status battery:" $status	
+		echo "Time rest:" $(date -d $date_rest +%kh:%Mm) "/" $perc_batery "%"			
+		echo "Current battery now:" $current_now "mA"		
+		echo "Consuming level:" $consuming_level
+	elif [[ $status == "Charging" ]]; then				
+		# echo "Verified in:" $(date +%kh:%mm:%Ss)
+		# echo "########################"
+		echo "Status battery:" $status	
+		echo "Percent to full:" $(((100 - $perc_batery))) "%"
+		echo "Current battery now:" $current_now "mA"		
+		echo "Consuming level:" $consuming_level
+	else
+		echo "ERROR"
+	fi			
 
 	echo "############################################"
 }
@@ -61,12 +88,6 @@ check()
 ## funcao principal
 main()
 {
-	clear	
-
-	if [[ `id -u` -ne 0 ]]; then
-		echo "[-] PRECISA DE ROOT PARA EXECUTAR" && exit 1
-	fi
-	
 	while [[ TRUE ]]; do
 		clear
 		check
