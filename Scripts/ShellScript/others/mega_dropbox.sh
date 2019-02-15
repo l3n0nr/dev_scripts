9494b0c09f9cb7be26c66273b43de788f71f453f
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-02-14 23:26:09 -0200

    verificar dropbox

--- Scripts/ShellScript/others/mega_dropbox.sh
+++ Scripts/ShellScript/others/mega_dropbox.sh
@@ -1,12 +1,61 @@
-comando=$(ps aux | grep megasync | grep pts/7)
+#!/usr/bin/env bash
 
-# echo "$comando"
+dropbox="0"
+mega="0"
 
-if [[ ! -e $comando ]]; then
-	echo "Nao existe"
-else
-	echo "existe"
-fi
+check_mega()
+{
+	ps aux | grep megasync | grep Sl >> /dev/null
+
+	if [[ $? == "0" ]]; then		
+		mega="1"
+	fi
+
+}
+
+check_dropbox()
+{
+	ps aux | grep dropbox | grep Sl >> /dev/null
+
+	if [[ $? == "0" ]]; then		
+		dropbox="1"
+	fi
+}
+
+echo_services()
+{
+	if [[ $dropbox == "0" ]]; then
+		# echo "Dropbox desligado"
+
+		dropbox start
+	else
+		# echo "Dropbox ligado"
+		
+		# dropbox stop
+		killall -9 dropbox >> /dev/null
+	fi
+
+	if [[ $mega == "0" ]]; then
+		# echo "Mega desligado"
+		
+		megasync &
+	else		
+		# echo "Mega ligado"
+		
+		killall -9 megasync 
+	fi
+}
+
+main()
+{
+	check_mega
+	check_dropbox
+	echo_services
+}
+
+main
+
+
 
 #dropbox start 
 #megasync 
