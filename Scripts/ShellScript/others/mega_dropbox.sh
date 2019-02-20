097ad51b3736a97c7f178891d65a84268a8a9ee7
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-02-20 15:43:35 -0300

    arquivo melhorado, funcionando start e stop

--- Scripts/ShellScript/others/mega_dropbox.sh
+++ Scripts/ShellScript/others/mega_dropbox.sh
@@ -1,48 +1,44 @@
 #!/usr/bin/env bash
 
-dropbox="0"
-mega="0"
-
 check_mega()
 {
+	mega="0"
+
 	ps aux | grep megasync | grep Sl >> /dev/null
 
 	if [[ $? == "0" ]]; then		
 		mega="1"
 	fi
 
+	if [[ $mega == "0" ]]; then
+		echo "- Subindo o MEGA"
+		megasync &
+	elif [[ $mega == "1" ]]; then
+		echo "- Derrubando o MEGA"
+		killall -9 megasync 
+	else		
+		echo "Error - Mega"
+	fi
 }
 
 check_dropbox()
 {
-	ps aux | grep dropbox | grep Sl >> /dev/null
+	dropbox="0"
+
+	ps aux | grep .dropbox-dist | grep Ssl >> /dev/null
 
 	if [[ $? == "0" ]]; then		
 		dropbox="1"
 	fi
-}
 
-echo_services()
-{
 	if [[ $dropbox == "0" ]]; then
-		# echo "Dropbox desligado"
-
-		dropbox start
-	else
-		# echo "Dropbox ligado"
-		
-		# dropbox stop
+		echo "- Subindo o Dropbox"
+		dropbox start >> /dev/null
+	elif [[ $dropbox == "1" ]]; then
+		echo "- Derrubando o Dropbox"
 		killall -9 dropbox >> /dev/null
-	fi
-
-	if [[ $mega == "0" ]]; then
-		# echo "Mega desligado"
-		
-		megasync &
 	else		
-		# echo "Mega ligado"
-		
-		killall -9 megasync 
+		echo "Error - Dropbox"
 	fi
 }
 
@@ -50,13 +46,6 @@ main()
 {
 	check_mega
 	check_dropbox
-	echo_services
 }
 
 main
-
-
-
-#dropbox start 
-#megasync 
-# dropbox stop
