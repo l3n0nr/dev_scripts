9ad8577171b6fc162583c2a258cff6b764804c6f
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-03-14 13:42:14 -0300

    novos sites adicionados e comentarios adicionados

--- Scripts/Python/leitor_url/main.sh
+++ Scripts/Python/leitor_url/main.sh
@@ -3,12 +3,22 @@
 #########################
 # data criacao = 13/03/19
 # ultima modif = 14/03/19
-# versao 	   = 0.16
+# versao 	   = 0.20
 #########################
 #
-array=(nasaspaceflight.com spaceflightnow.com cafeeciencia.com.br spacetoday.com.br ceticismo.net sempreupdate.com.br mensageirosideral.blogfolha.uol.com.br segurancalegal.com portalcafebrasil.com.br wemartians.com alociencia.com.br manualdomundo.com.br emagrecerdevez.com)
-# saida="/tmp/testando_twitter"
-saida="/home/lenonr/Dropbox/Arquivos/Twitter/posts"
+array=( nasaspaceflight.com spaceflightnow.com cafeeciencia.com.br \
+		spacetoday.com.br ceticismo.net sempreupdate.com.br \
+		mensageirosideral.blogfolha.uol.com.br segurancalegal.com portalcafebrasil.com.br \
+		wemartians.com alociencia.com.br manualdomundo.com.br \
+		emagrecerdevez.com teslarati.com rocketsciencebr.com )
+
+## saida oficial
+# saida="/home/lenonr/Dropbox/Arquivos/Twitter/posts"
+
+## saida de teste
+saida="/tmp/testando_twitter"
+
+## saida para organizacao dos links pelo script = nao comentar
 saida1="/tmp/testando_twitter1"
 #
 catch()
@@ -18,13 +28,7 @@ catch()
 
 		lynx --dump $link | \
 		jq --indent 0 '.[] | [.title.rendered, .link]' | \
-		sed -e 's/\(\["\|"\]\)//g' -e 's/"."/: /' >> $saida
-
-		# if [ $i == "0" ]; then 
-		# 	lynx --dump $link | \
-		# 	jq --indent 0 '.[] | [.title.rendered, .link]' | \
-		# 	sed -e 's/\(\["\|"\]\)//g' -e 's/"."/: /' >> $saida	
-		# fi		
+		sed -e 's/\(\["\|"\]\)//g' -e 's/"."/: /' >> $saida		
 	done
 }
 
