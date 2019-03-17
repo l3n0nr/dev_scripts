d13e61c77a9b9116c78e19a7a7127e00309556c5
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-03-17 16:12:04 -0300

    variavel post adicionada

--- Scripts/Python/leitor_url/main.sh
+++ Scripts/Python/leitor_url/main.sh
@@ -3,7 +3,7 @@
 #########################
 # data criacao = 13/03/19
 # ultima modif = 17/03/19
-# versao       = 0.45
+# versao       = 0.47
 #########################
 #
 array=( nasaspaceflight.com spaceflightnow.com cafeeciencia.com.br \
@@ -13,9 +13,11 @@ array=( nasaspaceflight.com spaceflightnow.com cafeeciencia.com.br \
         dragoesdegaragem.com avioesemusicas.com revistapesquisa.fapesp.br \
         almaobservatory.org emagrecerdevez.com ablc.org.br )
 
+post="/wp-json/wp/v2/posts/"
+
 ## saida do arquivo
-saida="/home/lenonr/Dropbox/Arquivos/Twitter/posts"
-# saida="/tmp/testando_twitter"
+# saida="/home/lenonr/Dropbox/Arquivos/Twitter/posts"
+saida="/tmp/testando_twitter"
 
 ## saida para organizacao dos links pelo script = NAO COMENTAR
 saida1="/tmp/testando_twitter1"
@@ -23,7 +25,7 @@ saida1="/tmp/testando_twitter1"
 catch()
 {
     for (( i = 0; i <= ${#array[@]}-1; i++ )); do 
-        link=${array[$i]}"/wp-json/wp/v2/posts/"
+        link=${array[$i]}"$post"
 
         lynx --dump $link | \
         jq --indent 0 '.[] | [.title.rendered, .link]' | \
