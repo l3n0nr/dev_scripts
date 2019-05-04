f3d6b7473a8ea195fe94a6f7a0c6a7b8ccc72143
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-05-03 22:47:20 -0300

    gera senha com ano

--- Scripts/Python/geradorsenhas/gerasenha.py
+++ Scripts/Python/geradorsenhas/gerasenha.py
@@ -2,12 +2,13 @@
 #
 # # Receitinha de bolo para criar senha simples e faceis de lembrar, XD.
 #
-# Criado em: 11/06/18
-# Ultima modificacao: 11/06/18
-# Autor: lenonr
+# DATA_CRIACAO: 	11/06/18
+# ULTM_MODIFICACAO: 03/04/19
+# AUTOR:			lenonr
 #
 ## HEAD
 import os, sys
+from datetime import date
 
 # limpando tela
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -18,12 +19,13 @@ def main():
 	nome_servico = raw_input("Digite o nome do servico que deseja criar a senha: ")
 	palavra_chave = raw_input("Digite a palavra_chave: ")	
 	qtd_caracteres = input("Digite a qtd_caracteres utilizar, partindo do nome do servico como base: ")
+	ano = str(date.today().year)
 	print
 	print "Opcoes de senhas"
-	print "{" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "*" + str(len(nome_servico)) + "}"
-	print "[" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "+" + str(len(nome_servico)) + "]"
-	print "(" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "#" + str(len(nome_servico)) + ")"
-	print "|" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "-" + str(len(nome_servico)) + "|"
+	print "{" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "*" + str(len(nome_servico)) + "_" + ano + "}"
+	print "[" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "+" + str(len(nome_servico)) + "_" + ano + "]"
+	print "(" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "#" + str(len(nome_servico)) + "_" + ano + ")"
+	print "|" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "-" + str(len(nome_servico)) + "_" + ano + "|"
 
 	pass
 
