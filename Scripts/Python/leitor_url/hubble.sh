b08180a88f8cff18a939a7c14d96dae2aa696d02
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-05-11 23:28:01 -0300

    perfil hubble adicionado

---
+++ Scripts/Python/leitor_url/hubble.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+#
+#########################
+# data criacao = 11/05/19
+# ultima modif = 11/05/19
+# versao       = 0.15
+#########################
+#
+## chamando arquivo externo com variaveis
+source variables.conf
+
+check_files()
+{
+    if [[ -e $output_titlehubble ]]; then
+        touch $output_titlehubble
+    else
+    	echo "" > $output_titlehubble
+    fi
+
+    if [[ -e $output_urlhubble ]]; then
+        touch $output_urlhubble
+    else
+    	echo "" > $output_urlhubble
+    fi
+
+    if [[ -e $saida_hubble ]]; then
+        touch $saida_hubble
+    else
+    	echo "" $saida_hubble
+    fi
+}
+
+catch_posts()
+{
+	for (( i = 0; i <= ${#array_hubble[@]}-1; i++ )); do 	
+		link="https://feeds-hourly.com/rss_2_json.php?nolimit=Yes&fd=http://hubblesite.org/feed/images/${array_hubble[$i]}?format=rss"
+
+		# TITLE
+		lynx --dump $link | \
+		jq --indent 0 '.items | .[] | .title' |
+		sed -e 's/\(\["\|"\]\)//g' | \
+		sed 's/"//g' | \
+		sed 's/null//g' > $output_titlehubble
+
+		# URL
+		lynx --dump $link | \
+		jq --indent 0 '.items | .[] | .url' |
+		sed -e 's/\(\["\|"\]\)//g' | \
+		sed 's/"//g' | \
+		sed 's/null//g' > $output_urlhubble
+
+		# COUNT LINES FILE
+		count=$(wc -l $output_titlehubble | awk '{print $1}')
+
+		for (( j = 1; j <= $count; j++ )); do
+			var1=$(sed -n $j'p' $output_titlehubble)
+			var2=$(sed -n $j'p' $output_urlhubble)
+
+			echo $var1 $var2 >> $saida_hubble
+		done
+	done
+}
+
+main()
+{
+	check_files
+	echo "" > $saida_hubble
+	catch_posts
+
+	awk 'NF' $saida_hubble > $trash_hubble
+	sort -R $trash_hubble > $saida_hubble	
+}
+
+main
