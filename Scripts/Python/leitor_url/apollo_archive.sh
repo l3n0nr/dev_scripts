7a75dc78c847611ee3333b84f25d37c1a4ef88ee
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-05-12 23:53:23 -0300

    perfil apollo_archive adicionado

---
+++ Scripts/Python/leitor_url/apollo_archive.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+#
+#########################
+# data criacao = 12/05/19
+# ultima modif = 12/05/19
+# versao       = 0.10
+#########################
+#
+## chamando arquivo externo com variaveis
+source variables.conf
+
+check_files()
+{
+    if [[ -e $output_titleapollo ]]; then
+        touch $output_titleapollo
+    else
+    	echo "" > $output_titleapollo
+    fi
+
+    if [[ -e $output_urlapollo ]]; then
+        touch $output_urlapollo
+    else
+    	echo "" > $output_urlapollo
+    fi
+
+    if [[ -e $saida_apollo ]]; then
+        touch $saida_apollo
+    else
+    	echo "" > $saida_apollo
+    fi
+
+    if [[ -e $trash_apollo ]]; then
+    	touch $trash_apollo
+    else
+    	echo "" > $trash_apollo
+    fi
+}
+
+catch_posts()
+{
+	link="https://public.opendatasoft.com/api/records/1.0/search/?dataset=project-apollo-archive&rows=$qtd"
+
+	# TITLE
+	lynx --dump $link | \
+	jq '. | .records | .[] | .fields | .photo_description' | \
+	sed 's/\(\["\|"\]\)//g' | \
+	sed 's/"//g' > $output_titleapollo
+
+	# URL
+	lynx --dump $link | \
+	jq '. | .records | .[] | .fields | .photo_url_o | .id' | \
+	sed 's/\(\["\|"\]\)//g' | \
+	sed 's/"//g' | \
+	sed 's|^|'$link_image'|' | \
+	sed 's|$|'"/download"'|' > $output_urlapollo
+
+	# COUNT LINES FILE
+	count=$(wc -l $output_titleapollo | awk '{print $1}')
+
+	for (( j = 1; j <= $count; j++ )); do
+		var1=$(sed -n $j'p' $output_titleapollo)
+		var2=$(sed -n $j'p' $output_urlapollo)
+
+		echo $var1 $var2 >> $saida_apollo
+	done
+}
+
+main()
+{
+	check_files
+	echo "" > $saida_apollo
+	catch_posts
+
+	awk 'NF' $saida_apollo > $trash_apollo
+	sort -R $trash_apollo > $saida_apollo	
+}
+
+main
--- Scripts/Python/leitor_url/main.sh
+++ Scripts/Python/leitor_url/main.sh
@@ -2,13 +2,14 @@
 #
 #########################
 # data criacao = 22/04/19
-# ultima modif = 11/05/19
-# versao       = 0.22
+# ultima modif = 12/05/19
+# versao       = 0.23
 #########################
 #
 ## chamando arquivo externo de variaveis
 source variables.conf
 
+## importando arquivos 
 check_wordpress()
 {
     source wordpress.sh
@@ -29,29 +30,38 @@ check_hubble()
     source hubble.sh
 }
 
+check_apollo()
+{
+    source apollo_archive.sh
+}
+
 # agrupando arquivos
 merge_files()
 {
     cat $saida_wordpress > $merge_posts
     cat $saida_rss >> $merge_posts    
     cat $saida_nasa >> $merge_posts  
-    cat $saida_hubble >> $merge_posts  
+    cat $saida_hubble >> $merge_posts 
+    cat $saida_apollo >> $merge_posts 
 }
 
-## misturando linhas
+## misturando linhas | NAO MODIFICAR
 sort_posts()
 {
     sort -R $merge_posts > $posts_twitter
 }
 
+## chamando arquivos externos
 check()
 {
     check_wordpress
     check_rss
     check_nasa
     check_hubble
+    check_apollo
 }
 
+## chamando funcao principal | NAO MODIFICAR
 main()
 {
     check
