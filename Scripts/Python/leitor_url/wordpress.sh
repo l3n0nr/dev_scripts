2920d684b01f87533443e7c2075b74c8ea18074e
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-05-12 23:52:54 -0300

    funcao check_files melhorada

--- Scripts/Python/leitor_url/wordpress.sh
+++ Scripts/Python/leitor_url/wordpress.sh
@@ -2,8 +2,8 @@
 #
 #########################
 # data criacao = 13/03/19
-# ultima modif = 25/04/19
-# versao       = 0.63
+# ultima modif = 12/05/19
+# versao       = 0.64
 #########################
 #
 ## chamando arquivo externo com variaveis
@@ -13,10 +13,14 @@ check_files()
 {
     if [[ -e $saida_wordpress ]]; then
         touch $saida_wordpress
+    else
+        echo "" > $saida_wordpress
     fi
 
     if [[ -e $trash_wordpress ]]; then
         touch $trash_wordpress
+    else
+        echo "" > $trash_wordpress
     fi
 }
 
