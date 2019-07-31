ec460e1a80f9061702d872222d099091fd2d0381
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-07-31 10:33:11 -0300

    arquivo melhorado, usando ibam

--- Scripts/ShellScript/bateria/mah_to_minutes.sh
+++ Scripts/ShellScript/bateria/mah_to_minutes.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 #
-## DESCRICAO: Calcula o tempo de duracao da bateria de acordo com mAh
+#######################
 ## DATA CRIA: 31/07/19
 ## ULT_MODIF: 31/07/19
+#######################
 #
-# current_now="$(($(cat /sys/class/power_supply/BAT0/current_now) / 100))"
-current_now="1500"
-mah="$(($(cat /sys/class/power_supply/BAT0/charge_full) / 1000))"
-#
-check_mah()
+time_rest()
 {
-	battery_life="$((( $mah/$current_now )))"
+	time_rest_battery="$(ibam --totalbatteryadaptive | grep "Adapted total battery time:"| awk {'print $5'})"
 
-	echo "Tempo apromixado de bateria" $battery_life "hora(s)."
+	echo "Tempo apromixado:" $time_rest_battery
 }
 
 main()
 {
-	check_mah
+	time_rest
 }
 
 ## chamando funcao principal
