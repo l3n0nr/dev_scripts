d0711a0bc732272ff48313ece48c8a0f0bb6f4dd
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-12-31 10:17:45 -0300

    arquivo melhorado

--- Scripts/Python/geradorsenhas/gerasenha.py
+++ Scripts/Python/geradorsenhas/gerasenha.py
@@ -3,37 +3,42 @@
 # # Receitinha de bolo para criar senha simples e faceis de lembrar, XD.
 #
 # DATA_CRIACAO: 	11/06/18
-# ULTM_MODIFICACAO: 03/04/19
+# ULTM_MODIFICACAO: 31/12/19
 # AUTOR:			lenonr
 #
-## HEAD
-import os, sys
+# HEAD
+import os
+import sys
 from datetime import date
 
 # limpando tela
 os.system('cls' if os.name == 'nt' else 'clear')
 
-## BODY
-## funcoes
+# BODY
+# funcoes
+
+
 def main():
-	nome_servico = raw_input("Digite o nome do servico que deseja criar a senha: ")
-	palavra_chave = raw_input("Digite a palavra_chave: ")	
-	qtd_caracteres = input("Digite a qtd_caracteres utilizar, partindo do nome do servico como base: ")
-	ano = str(date.today().year)
-	print
-	print "Opcoes de senhas"
-	print "{" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "*" + str(len(nome_servico)) + "_" + ano + "}"
-	print "[" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "+" + str(len(nome_servico)) + "_" + ano + "]"
-	print "(" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "#" + str(len(nome_servico)) + "_" + ano + ")"
-	print "|" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "-" + str(len(nome_servico)) + "_" + ano + "|"
+    nome_servico = raw_input(
+        "Digite o nome do servico que deseja criar a senha: ")
+    palavra_chave = raw_input("Digite a palavra_chave: ")
+    qtd_caracteres = input(
+        "Digite a qtd_caracteres utilizar, partindo do nome do servico como base: ")
+    ano = str(date.today().year)
+    print "Opcoes de senhas"
+    print "{" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "*" + str(len(nome_servico)) + "_" + ano + "}"
+    print "[" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "+" + str(len(nome_servico)) + "_" + ano + "]"
+    print "(" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "#" + str(len(nome_servico)) + "_" + ano + ")"
+    print "|" + nome_servico.upper()[0:qtd_caracteres] + palavra_chave + "-" + str(len(nome_servico)) + "_" + ano + "|"
 
-	pass
+    pass
 
-## executando script
+
+# executando script
 print("Gerador de senhas - v1")
 print("======================")
 # chamando funcao
 main()
 print("======================")
 
-## FOOTER
+# FOOTER
