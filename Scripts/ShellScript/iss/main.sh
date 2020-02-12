134c366727b7902f48980fe9b0525c44a7005c72
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-02-12 15:44:22 -0300

    file otimized and working

--- Scripts/ShellScript/iss/main.sh
+++ Scripts/ShellScript/iss/main.sh
@@ -2,8 +2,8 @@
 #
 #######################
 # dat_criacao: 03/12/19
-# ult_modific: 17/12/19
-# versao: 		 0.20
+# ult_modific: 12/01/20
+# versao: 		 0.30
 #######################
 #
 # Reference: http://api.open-notify.org/
@@ -20,8 +20,8 @@ pass_ISS()
 
 crew_ISS()
 {
-	output_crewISS="/tmp/crew_iss.txt"
 	output_crewISS_tmp="/tmp/crew_iss.txt.tmp"
+	output_crewISS_nmb="/tmp/crew_ISS_number.txt"
 
 	people="http://api.open-notify.org/astros.json"
 	data=$(date)
@@ -29,15 +29,26 @@ crew_ISS()
 	# call alias external path
 	call_twitt="/home/lenonr/Github/dev_scripts/Scripts/Python/twitter_bot/v1.py -t"
 
+	## name people in space
 	lynx --dump $people | \
 	jq '.people[].name' | \
 	sed -e 's/^"//g' | sed -e 's/"/, /g' | \
 	tr -d '\n' > $output_crewISS_tmp
 
-	printf 'Astronauts in space now: ' && sed -e 's/..$//g' $output_crewISS_tmp && printf ". Source: " && printf $people > $output_crewISS
+	## number people in space
+	lynx --dump $people | \
+	jq '.number' 		| \
+	tr -d '\n' > $output_crewISS_nmb
+
+	## output in display
+	output_crewISS=$(printf '#ISS_NOW - ' && \
+					  cat $output_crewISS_nmb && \
+					  printf ' astronauts in space: ' && \
+					  sed -e 's/..$//g' $output_crewISS_tmp && \
+					  printf ". Source: " && printf $people && echo " (BOT CHECK:"$(date +%d-%h_%H:%M)')')
 	
-	cat $output_crewISS
-	# python $call_twitt 'cat $output_crewISS'
+	echo $output_crewISS
+	python $call_twitt "$(echo $output_crewISS)"
 }
 
 main()
