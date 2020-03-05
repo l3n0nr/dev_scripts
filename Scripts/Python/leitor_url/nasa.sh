daded4e3ef80b8d5b8edb5a0ffbdeb7bb835478b
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-03-04 22:57:09 -0300

    funcoes audio + video funcionando

--- Scripts/Python/leitor_url/nasa.sh
+++ Scripts/Python/leitor_url/nasa.sh
@@ -2,8 +2,8 @@
 #
 #########################
 # data criacao = 28/04/19
-# ultima modif = 11/05/19
-# versao       = 0.21
+# ultima modif = 04/03/20
+# versao       = 0.30
 #########################
 #
 ## chamando arquivo externo com variaveis
@@ -11,77 +11,168 @@ source variables.conf
 
 check_files()
 {
-    if [[ -e $output_title ]]; then
-        touch $output_title
+	# image
+    if [[ -e $output_title_image ]]; then
+        touch $output_title_image
     else
-    	echo "" > $output_title
+    	echo "" > $output_title_image
     fi
 
-    if [[ -e $output_url ]]; then
-        touch $output_url
+    if [[ -e $output_url_image ]]; then
+        touch $output_url_image
     else
-    	echo "" > $output_url
+    	echo "" > $output_url_image
     fi
 
-    if [[ -e $saida_nasa ]]; then
-        touch $saida_nasa
+    if [[ -e $saida_nasa_image ]]; then
+        touch $saida_nasa_image
     else
-    	echo "" > $saida_nasa
+    	echo "" > $saida_nasa_image
+    fi
+
+    # audio
+    if [[ -e $output_title_audio ]]; then
+        touch $output_title_audio
+    else
+    	echo "" > $output_title_audio
+    fi
+
+    if [[ -e $output_url_audio ]]; then
+        touch $output_url_audio
+    else
+    	echo "" > $output_url_audio
+    fi
+
+    if [[ -e $saida_nasa_audio ]]; then
+        touch $saida_nasa_audio
+    else
+    	echo "" > $saida_nasa_audio
+    fi
+
+    # video
+    if [[ -e $output_title_video ]]; then
+        touch $output_title_video
+    else
+    	echo "" > $output_title_video
+    fi
+
+    if [[ -e $output_url_video ]]; then
+        touch $output_url_video
+    else
+    	echo "" > $output_url_video
+    fi
+
+    if [[ -e $saida_nasa_video ]]; then
+        touch $saida_nasa_video
+    else
+    	echo "" > $saida_nasa_video
     fi
 }
 
 catch_posts()
 {
-	for (( i = 0; i <= ${#array_nasa[@]}-1; i++ )); do 			
-		link="https://images-api.nasa.gov/search?q=${array_nasa[$i]}&media_type=image"
+	image()
+	{
+		## IMAGE
+		link_image="https://images-api.nasa.gov/search?q=${array_nasa[$i]}&media_type=image"
 
-		# TITLE
-		lynx --dump $link | \
+		# title
+		lynx --dump $link_image | \
 		jq --indent 0 '.[] | .items | .[] | {data} | .[] | .[] | .title' | \
 		sed -e 's/\(\["\|"\]\)//g' | \
-		sed 's/"//g' > $output_title
+		sed 's/"//g' > $output_title_image
 
-		# # URL - image
-		lynx --dump $link | \
+		# url
+		lynx --dump $link_image | \
 		jq --indent 0 '.[] | .items | .[] | .links | .[] | .href' | \
 		sed -e 's/\(\["\|"\]\)//g' | \
-		sed 's/"//g' > $output_url
+		sed 's/"//g' > $output_url_image
 
-		## VERIFICAR - audio
-		# link="https://images-api.nasa.gov/search?q=${array_nasa[$i]}&media_type=audio"
-		
-		# URL - audio
-		# lynx --dump $link | \
-		# jq --indent 0 '.[] | .items | .[] | .href' | \
-		# sed -e 's/\(\["\|"\]\)//g' | \
-		# sed 's/"//g' > $output_url
+		count_image=$(wc -l $output_title_image | awk '{print $1}')
 
-		# ## VERIFICAR - video
-		# link="https://images-api.nasa.gov/search?q=${array_nasa[$i]}&media_type=video"
-		
-		# # URL - video
-		# lynx --dump $link | \
-		# jq --indent 0 '.[] | .items | .[] | .href' | \
-		# sed -e 's/\(\["\|"\]\)//g' | \
-		# sed 's/"//g' > $output_url
+		for (( j = 1; j <= $count_image; j++ )); do
+			var1=$(sed -n $j'p' $output_title_image)
+			var2=$(sed -n $j'p' $output_url_image)
 
-		# COUNT LINES FILE
-		count=$(wc -l $output_title | awk '{print $1}')
-
-		for (( j = 1; j <= $count; j++ )); do
-			var1=$(sed -n $j'p' $output_title)
-			var2=$(sed -n $j'p' $output_url)
-
-			echo $var1 $var2 >> $saida_nasa
+			echo $var1 $var2 >> $saida_nasa_image
 		done
+	}
+	
+	audio()
+	{
+		## AUDIO
+		link_audio="https://images-api.nasa.gov/search?q=${array_nasa[$i]}&media_type=audio"
+
+		# title
+		lynx --dump $link_audio | \
+		jq --indent 0 '.[] | .items | .[] | {data} | .[] | .[] | .title' | \
+		sed -e 's/\(\["\|"\]\)//g' | \
+		sed 's/"//g' > $output_title_audio
+		
+		# url
+		lynx --dump $link_audio | \
+		jq --indent 0 '.[] | .items | .[] | .href' | \
+		sed -e 's/\(\["\|"\]\)//g' | \
+		sed 's/"//g' > $output_url_audio
+
+		count_audio=$(wc -l $output_title_audio | awk '{print $1}')
+
+		for (( j = 1; j <= $count_audio; j++ )); do
+			var1=$(sed -n $j'p' $output_title_audio)
+			var2=$(sed -n $j'p' $output_url_audio)
+
+			echo $var1 $var2 >> $saida_nasa_audio
+		done
+	}	
+
+	video()
+	{
+		## VIDEO
+		link_video="https://images-api.nasa.gov/search?q=${array_nasa[$i]}&media_type=video"
+		
+		# title
+		lynx --dump $link_video | \
+		jq --indent 0 '.[] | .items | .[] | {data} | .[] | .[] | .title' | \
+		sed -e 's/\(\["\|"\]\)//g' | \
+		sed 's/"//g' > $output_title_video
+
+		# url
+		lynx --dump $link_video | \
+		jq --indent 0 '.[] | .items | .[] | .href' | \
+		sed -e 's/\(\["\|"\]\)//g' | \
+		sed 's/"//g' > $output_url_video
+
+		count_video=$(wc -l $output_title_video | awk '{print $1}')
+
+		for (( j = 1; j <= $count_video; j++ )); do
+			var1=$(sed -n $j'p' $output_title_video)
+			var2=$(sed -n $j'p' $output_url_video)
+
+			echo $var1 $var2 >> $saida_nasa_video
+		done
+	}			
+
+	for (( i = 0; i <= ${#array_nasa[@]}-1; i++ )); do 
+		image
+		audio
+		video			
 	done
+
+	# image + audio + video
+	cat $saida_nasa_image $saida_nasa_audio $saida_nasa_video >> $saida_nasa
 }
 
 main()
 {
 	check_files
+
 	printf "" > $saida_nasa
+	printf "" > $saida_nasa_image
+	printf "" > $saida_nasa_audio
+	printf "" > $saida_nasa_video
+
 	catch_posts
+
 	awk 'NF' $saida_nasa > $trash_nasa
 	sort -R $trash_nasa > $saida_nasa
 }
