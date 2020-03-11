54bfb9201e3a1bc5f7ec747452c5d3054d8b1a45
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-03-10 21:41:01 -0300

    verificacao data melhorada

--- Scripts/ShellScript/iss/main.sh
+++ Scripts/ShellScript/iss/main.sh
@@ -2,22 +2,31 @@
 #
 #######################
 # dat_criacao: 03/12/19
-# ult_modific: 27/02/20
-# versao: 		 0.36
+# ult_modific: 10/03/20
+# versao: 		 0.40
 #######################
 #
+source variables.conf
+
+check_files()
+{
+	if [[ ! -e $validation_iss ]]; then
+		touch $validation_iss
+	fi
+
+	if [[ ! -e $output_crewISS_file ]]; then
+		touch $output_crewISS_file
+	fi
+}
+
 pass_ISS()
 {
 	## call external file
-	source location.conf
-
-	output_pass="/tmp/iss_pass.txt"	
+	source location.conf	
 
 	if [[ -z $output_pass ]]; then
 		touch $output_pass
-	fi	
-
-	pass="http://api.open-notify.org/iss-pass.json?lat=$lat&lon=$lon"
+	fi		
 	
 	lynx --dump $pass | \
 	jq '.response[]' > $output_pass
@@ -29,41 +38,49 @@ pass_ISS()
 }
 
 crew_ISS()
-{
-	output_crewISS_tmp="/tmp/crew_iss.txt.tmp"
-	output_crewISS_nmb="/tmp/crew_ISS_number.txt"
+{	
+	check_files
 
-	people="http://api.open-notify.org/astros.json"
-	data=$(date)
+	keyword="ISS_NOW"
+	validation_iss_check=$(cat $validation_iss)
 
-	# call alias external path
-	call_twitt="/home/lenonr/Github/dev_scripts/Scripts/Python/twitter_bot/v1.py -t"
+	if [[ -e $output_crewISS_file ]]; then
+		if [[ $check_date == $validation_iss_check ]]; then
+			echo "NOT CHECK -" $check_date >> $log_validation_iss
+		elif [[ $check_date != $validation_iss_check ]]; then
+			## name people in space
+			lynx --dump $people | \
+			jq '.people[].name' | \
+			sed -e 's/^"//g' | sed -e 's/"/, /g' | \
+			tr -d '\n' > $output_crewISS_tmp
 
-	## name people in space
-	lynx --dump $people | \
-	jq '.people[].name' | \
-	sed -e 's/^"//g' | sed -e 's/"/, /g' | \
-	tr -d '\n' > $output_crewISS_tmp
+			## number people in space
+			lynx --dump $people | \
+			jq '.number' 		| \
+			tr -d '\n' > $output_crewISS_nmb
 
-	## number people in space
-	lynx --dump $people | \
-	jq '.number' 		| \
-	tr -d '\n' > $output_crewISS_nmb
+			## output in display
+			output_crewISS=$(printf '#ISS_NOW - ' && \
+							  cat $output_crewISS_nmb && \
+							  printf ' astronauts in space: ' && \
+							  sed -e 's/..$//g' $output_crewISS_tmp && \
+							  printf "." && echo " (BOT CHECK:"$(date +%d-%h_%H:%M)')')	
+			
+			echo $check_date > $validation_iss
+			echo $output_crewISS > $output_crewISS_file
 
-	## output in display
-	output_crewISS=$(printf '#ISS_NOW - ' && \
-					  cat $output_crewISS_nmb && \
-					  printf ' astronauts in space: ' && \
-					  sed -e 's/..$//g' $output_crewISS_tmp && \
-					  printf "." && echo " (BOT CHECK:"$(date +%d-%h_%H:%M)')')
-	
-	# echo $output_crewISS
-	python $call_twitt "$(echo $output_crewISS)"
+			## TESTES
+			cat $output_crewISS_file
+			# python $call_twitt "$(echo $output_crewISS)"
+		else
+			echo "ERROR -" $check_date >> $log_validation_iss
+		fi		
+	fi	
 }
 
 main()
 {
-	clear
+	clear	
 
 	if [[ $1 == "pass" ]]; then
 		pass_ISS
